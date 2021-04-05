e21adacdb5a0d54d0426325a6416e3494a596799
beanbeanjuice <beanbeanjuice@outlook.com>
2021-04-05 16:29:46 -0400

    Created the SkipCommand

---
+++ src/main/java/com/beanbeanjuice/command/music/SkipCommand.java
@@ -0,0 +1,121 @@
+package com.beanbeanjuice.command.music;
+
+import com.beanbeanjuice.utility.command.CommandContext;
+import com.beanbeanjuice.utility.command.ICommand;
+import com.beanbeanjuice.utility.command.usage.Usage;
+import com.beanbeanjuice.utility.command.usage.categories.CategoryType;
+import com.beanbeanjuice.utility.lavaplayer.GuildMusicManager;
+import com.beanbeanjuice.utility.lavaplayer.PlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+/**
+ * A command used for skipping the current song.
+ *
+ * @author beanbeanjuice
+ */
+public class SkipCommand implements ICommand {
+
+    @Override
+    public void handle(CommandContext ctx, ArrayList<String> args, User user, GuildMessageReceivedEvent event) {
+
+        event.getMessage().delete().queue();
+
+        final Member self = ctx.getSelfMember();
+        final GuildVoiceState selfVoiceState = self.getVoiceState();
+
+        if (!selfVoiceState.inVoiceChannel()) {
+            event.getChannel().sendMessage(botMustBeInVoiceChannelEmbed()).queue();
+            return;
+        }
+
+        if (selfVoiceState.inVoiceChannel()) {
+            if (!event.getMember().getVoiceState().inVoiceChannel()) {
+                event.getChannel().sendMessage(user.getAsMention()).queue(e -> {
+                    e.delete().queue();
+                });
+                event.getChannel().sendMessage(mustBeInVoiceChannelEmbed()).queue();
+                return;
+            }
+
+            GuildMusicManager musicManager = PlayerManager.getInstance().getMusicManager(ctx.getGuild());
+            AudioPlayer audioPlayer = musicManager.audioPlayer;
+
+            if (audioPlayer.getPlayingTrack() == null) {
+                event.getChannel().sendMessage(noTrackPlayingEmbed()).queue();
+                return;
+            }
+
+            musicManager.scheduler.nextTrack();
+            event.getChannel().sendMessage(successEmbed()).queue();
+
+
+        }
+
+    }
+
+    private MessageEmbed botMustBeInVoiceChannelEmbed() {
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setDescription("I'm not currently in a voice channel.");
+        embedBuilder.setColor(Color.red);
+        return embedBuilder.build();
+    }
+
+    private MessageEmbed noTrackPlayingEmbed() {
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+
+        embedBuilder.setDescription("There is no track currently playing.");
+        embedBuilder.setColor(Color.orange);
+
+        return embedBuilder.build();
+    }
+
+    private MessageEmbed mustBeInVoiceChannelEmbed() {
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setDescription("Sorry, you must be in a voice channel to use this command.");
+        embedBuilder.setColor(Color.red);
+        return embedBuilder.build();
+    }
+
+    private MessageEmbed successEmbed() {
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setDescription("Successfully skipped the track.");
+        embedBuilder.setColor(Color.green);
+
+        return embedBuilder.build();
+    }
+
+    @Override
+    public String getName() {
+        return "skip";
+    }
+
+    @Override
+    public ArrayList<String> getAliases() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public String getDescription() {
+        return "Skips the current song";
+    }
+
+    @Override
+    public Usage getUsage() {
+        return new Usage();
+    }
+
+    @Override
+    public CategoryType getCategoryType() {
+        return CategoryType.MUSIC;
+    }
+
+}
