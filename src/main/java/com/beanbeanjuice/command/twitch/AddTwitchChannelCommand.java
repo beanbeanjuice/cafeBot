ced388e1ed7a8ab0cac7a4cadfccf6dc0dd3b75b
beanbeanjuice <beanbeanjuice@outlook.com>
2021-04-05 20:00:59 -0400

    Added twitch channel commands

---
+++ src/main/java/com/beanbeanjuice/command/twitch/AddTwitchChannelCommand.java
@@ -0,0 +1,75 @@
+package com.beanbeanjuice.command.twitch;
+
+import com.beanbeanjuice.main.BeanBot;
+import com.beanbeanjuice.utility.command.CommandContext;
+import com.beanbeanjuice.utility.command.ICommand;
+import com.beanbeanjuice.utility.command.usage.Usage;
+import com.beanbeanjuice.utility.command.usage.categories.CategoryType;
+import com.beanbeanjuice.utility.command.usage.types.CommandType;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+/**
+ * A command to add a twitch channel.
+ *
+ * @author beanbeanjuice
+ */
+public class AddTwitchChannelCommand implements ICommand {
+
+    @Override
+    public void handle(CommandContext ctx, ArrayList<String> args, User user, GuildMessageReceivedEvent event) {
+
+        if (!BeanBot.getGeneralHelper().isModerator(event.getMember(), event.getGuild(), event)) {
+            return;
+        }
+
+        if (!BeanBot.getGuildHandler().getCustomGuild(event.getGuild().getId()).addTwitchChannel(args.get(0))) {
+            EmbedBuilder embedBuilder = new EmbedBuilder();
+            embedBuilder.setColor(Color.red);
+            embedBuilder.setDescription("Unable to add the twitch channel.");
+            embedBuilder.setAuthor("Error Adding Twitch Channel");
+            event.getChannel().sendMessage(embedBuilder.build()).queue();
+            return;
+        }
+
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setAuthor("Successfully Added Twitch Channel");
+        embedBuilder.setColor(BeanBot.getGeneralHelper().getRandomColor());
+        embedBuilder.setDescription("Successfully added `" + args.get(0) + "`!");
+        event.getChannel().sendMessage(embedBuilder.build()).queue();
+
+    }
+
+    @Override
+    public String getName() {
+        return "add-twitch-channel";
+    }
+
+    @Override
+    public ArrayList<String> getAliases() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("addtwitchchannel");
+        return arrayList;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Add a twitch channel to the live channel!";
+    }
+
+    @Override
+    public Usage getUsage() {
+        Usage usage = new Usage();
+        usage.addUsage(CommandType.TEXT, "Twitch Username", true);
+        return usage;
+    }
+
+    @Override
+    public CategoryType getCategoryType() {
+        return CategoryType.TWITCH;
+    }
+}
