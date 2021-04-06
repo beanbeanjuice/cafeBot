0a94f87b1c830505ce908cc221f571d6ae6e0a98
beanbeanjuice <beanbeanjuice@outlook.com>
2021-04-06 12:02:14 -0400

    Updated the kick command

--- src/main/java/com/beanbeanjuice/command/moderation/KickCommand.java
+++ src/main/java/com/beanbeanjuice/command/moderation/KickCommand.java
@@ -7,6 +7,7 @@ import com.beanbeanjuice.utility.command.usage.Usage;
 import com.beanbeanjuice.utility.command.usage.categories.CategoryType;
 import com.beanbeanjuice.utility.command.usage.types.CommandType;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
@@ -27,7 +28,7 @@ public class KickCommand implements ICommand {
 
         event.getMessage().delete().queue();
 
-        if (!BeanBot.getGeneralHelper().isModerator(event.getMember(), event.getGuild(), event)) {
+        if (!BeanBot.getGeneralHelper().isModerator(event.getMember(), event.getChannel(), Permission.KICK_MEMBERS)) {
             return;
         }
 
--- src/main/java/com/beanbeanjuice/utility/helper/GeneralHelper.java
+++ src/main/java/com/beanbeanjuice/utility/helper/GeneralHelper.java
@@ -79,39 +79,43 @@ public class GeneralHelper {
     /**
      * Checks if the specified {@link Member} is an administrator or has the moderator role.
      * @param member The {@link Member} to be checked.
-     * @param guild The {@link Guild} the {@link Member} is in.
-     * @param event The {@link GuildMessageReceivedEvent} that was sent.
+     * @param channel The {@link TextChannel} the {@link GuildMessageReceivedEvent} is in.
+     * @param permission The {@link Permission} to check for.
      * @return Whether or not the {@link Member} can run the command.
      */
     @NotNull
-    public Boolean isModerator(@NotNull Member member, @NotNull Guild guild, @NotNull GuildMessageReceivedEvent event) {
+    public Boolean isModerator(@NotNull Member member, @NotNull TextChannel channel, @NotNull Permission permission) {
 
         if (member.hasPermission(Permission.ADMINISTRATOR)) {
             return true;
         }
 
-        CustomGuild customGuild = BeanBot.getGuildHandler().getCustomGuild(guild);
+//        CustomGuild customGuild = BeanBot.getGuildHandler().getCustomGuild(guild);
 
-        if (customGuild.getModeratorRole() == null) {
-            EmbedBuilder embedBuilder = new EmbedBuilder();
-            embedBuilder.setAuthor("No Moderator Role Set");
-            embedBuilder.setColor(Color.red);
-            embedBuilder.setDescription("No moderator role has been set. Please check the help command for more information.");
-            event.getChannel().sendMessage(embedBuilder.build()).queue();
-            return false;
-        }
+        // TODO: Possibly use this in the future.
+//        if (customGuild.getModeratorRole() == null) {
+//            EmbedBuilder embedBuilder = new EmbedBuilder();
+//            embedBuilder.setAuthor("No Moderator Role Set");
+//            embedBuilder.setColor(Color.red);
+//            embedBuilder.setDescription("No moderator role has been set. Please check the help command for more information.");
+//            event.getChannel().sendMessage(embedBuilder.build()).queue();
+//            return false;
+//        }
 
-        if (!member.getRoles().contains(customGuild.getModeratorRole())) {
+        if (!member.hasPermission(permission)) {
             EmbedBuilder embedBuilder = new EmbedBuilder();
             embedBuilder.setAuthor("No Permission");
             embedBuilder.setColor(Color.red);
-            embedBuilder.setDescription("You don't have permission to run this command.");
-            event.getChannel().sendMessage(embedBuilder.build()).queue();
+            embedBuilder.setDescription("You don't have the permission `" + permission.getName() + "` to run this command.");
+            channel.sendMessage(embedBuilder.build()).queue();
             return false;
         }
         return true;
     }
 
+    /**
+     * @return The SQLServerError {@link MessageEmbed}.
+     */
     public MessageEmbed sqlServerError() {
         EmbedBuilder embedBuilder = new EmbedBuilder();
         embedBuilder.setColor(Color.red);
