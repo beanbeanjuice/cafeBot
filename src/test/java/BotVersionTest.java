5c801dee51670dddbfcd8c95a51e9312928ad3fe
beanbeanjuice <beanbeanjuice@outlook.com>
2021-05-01 23:35:13 -0400

    Added the Unit Testing for the Bot Version
    This can help keep the versions intact, making sure that when a release
    is made, the version is changed in the code.

---
+++ src/test/java/BotVersionTest.java
@@ -0,0 +1,14 @@
+import com.beanbeanjuice.main.BeanBot;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BotVersionTest {
+
+    @Test
+    @DisplayName("Bot Version Test")
+    public void test() {
+        Assertions.assertEquals(BeanBot.getBotVersion(), "v1.0.8", "Bot Version Incorrect!");
+    }
+
+}
