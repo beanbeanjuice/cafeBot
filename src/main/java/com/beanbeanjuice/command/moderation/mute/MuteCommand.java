a8d490b8d9d40579dd4af9336a9ca70205929e52
beanbeanjuice <beanbeanjuice@outlook.com>
2021-05-06 04:35:37 -0400

    Fixed the Mute Command
    Additionally, some code was cleaned up.

--- src/main/java/com/beanbeanjuice/command/moderation/mute/MuteCommand.java
+++ src/main/java/com/beanbeanjuice/command/moderation/mute/MuteCommand.java
@@ -9,10 +9,12 @@ import com.beanbeanjuice.utility.command.usage.types.CommandType;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.exceptions.HierarchyException;
+import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -26,9 +28,6 @@ public class MuteCommand implements ICommand {
 
     @Override
     public void handle(CommandContext ctx, ArrayList<String> args, User user, GuildMessageReceivedEvent event) {
-
-        event.getMessage().delete().queue();
-
         if (!BeanBot.getGeneralHelper().checkPermission(event.getMember(), event.getChannel(), Permission.MANAGE_ROLES)) {
             return;
         }
@@ -36,11 +35,7 @@ public class MuteCommand implements ICommand {
         Role mutedRole = BeanBot.getGuildHandler().getCustomGuild(event.getGuild()).getMutedRole();
 
         if (mutedRole == null) {
-            EmbedBuilder embedBuilder = new EmbedBuilder();
-            embedBuilder.setColor(Color.red);
-            embedBuilder.setAuthor("Error Muting User");
-            embedBuilder.setDescription("A muted role has not been set. Please check the help command.");
-            event.getChannel().sendMessage(embedBuilder.build()).queue();
+            event.getChannel().sendMessage(noMutedRoleEmbed()).queue();
             return;
         }
 
@@ -49,21 +44,38 @@ public class MuteCommand implements ICommand {
         try {
             event.getGuild().addRoleToMember(punishee, mutedRole).queue();
         } catch (HierarchyException e) {
-            EmbedBuilder embedBuilder = new EmbedBuilder();
-            embedBuilder.setAuthor("Hierarchy Error");
-            embedBuilder.setColor(Color.red);
-            embedBuilder.setDescription("The bot needs to have a higher role than the user you are trying to mute!");
-            event.getChannel().sendMessage(embedBuilder.build()).queue();
+            event.getChannel().sendMessage(hierarchyErrorEmbed()).queue();
             return;
         }
 
+        event.getChannel().sendMessage(successfulMuteEmbed()).queue();
+    }
+
+    @NotNull
+    public MessageEmbed noMutedRoleEmbed() {
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setColor(Color.red);
+        embedBuilder.setAuthor("Error Muting User");
+        embedBuilder.setDescription("A muted role has not been set. Please check the help command.");
+        return embedBuilder.build();
+    }
+
+    @NotNull
+    public MessageEmbed successfulMuteEmbed() {
         EmbedBuilder embedBuilder = new EmbedBuilder();
         embedBuilder.setAuthor("Muted User");
         embedBuilder.setColor(BeanBot.getGeneralHelper().getRandomColor());
         embedBuilder.setDescription("Muted " + punishee.getAsMention() + ".");
+        return embedBuilder.build();
+    }
 
-        event.getChannel().sendMessage(embedBuilder.build()).queue();
-
+    @NotNull
+    public MessageEmbed hierarchyErrorEmbed() {
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setAuthor("Hierarchy Error");
+        embedBuilder.setColor(Color.red);
+        embedBuilder.setDescription("The bot needs to have a higher role than the user you are trying to mute!");
+        return embedBuilder.build();
     }
 
     @Override
