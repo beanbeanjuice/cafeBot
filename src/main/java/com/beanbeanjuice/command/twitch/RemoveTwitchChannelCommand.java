0f647624892ce3cb4be5816e3572d50b51bf1831
beanbeanjuice <beanbeanjuice@outlook.com>
2021-05-06 05:09:51 -0400

    Fixed Twitch Commands
    Additionally, some code was cleaned up.

--- src/main/java/com/beanbeanjuice/command/twitch/RemoveTwitchChannelCommand.java
+++ src/main/java/com/beanbeanjuice/command/twitch/RemoveTwitchChannelCommand.java
@@ -7,8 +7,10 @@ import com.beanbeanjuice.utility.command.usage.Usage;
 import com.beanbeanjuice.utility.command.usage.categories.CategoryType;
 import com.beanbeanjuice.utility.command.usage.types.CommandType;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -22,28 +24,34 @@ public class RemoveTwitchChannelCommand implements ICommand {
 
     @Override
     public void handle(CommandContext ctx, ArrayList<String> args, User user, GuildMessageReceivedEvent event) {
-
-        event.getMessage().delete().queue();
-
         if (!BeanBot.getGeneralHelper().isModerator(event.getMember(), event.getGuild(), event)) {
             return;
         }
 
         if (!BeanBot.getGuildHandler().getCustomGuild(event.getGuild().getId()).removeTwitchChannel(args.get(0))) {
-            EmbedBuilder embedBuilder = new EmbedBuilder();
-            embedBuilder.setColor(Color.red);
-            embedBuilder.setDescription("Unable to remove the twitch channel. The twitch channel may not be added.");
-            embedBuilder.setAuthor("Error Removing Twitch Channel");
-            event.getChannel().sendMessage(embedBuilder.build()).queue();
+            event.getChannel().sendMessage(notAddedEmbed()).queue();
             return;
         }
 
+        event.getChannel().sendMessage(successfulRemoveEmbed(args.get(0))).queue();
+    }
+
+    @NotNull
+    private MessageEmbed successfulRemoveEmbed(@NotNull String twitchName) {
         EmbedBuilder embedBuilder = new EmbedBuilder();
         embedBuilder.setAuthor("Successfully Removed the Twitch Channel");
         embedBuilder.setColor(BeanBot.getGeneralHelper().getRandomColor());
-        embedBuilder.setDescription("Successfully removed `" + args.get(0) + "`!");
-        event.getChannel().sendMessage(embedBuilder.build()).queue();
+        embedBuilder.setDescription("Successfully removed `" + twitchName + "`!");
+        return embedBuilder.build();
+    }
 
+    @NotNull
+    private MessageEmbed notAddedEmbed() {
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setColor(Color.red);
+        embedBuilder.setDescription("Unable to remove the twitch channel. The twitch channel may not be added.");
+        embedBuilder.setAuthor("Error Removing Twitch Channel");
+        return embedBuilder.build();
     }
 
     @Override
