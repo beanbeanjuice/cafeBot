cd5ea4bca020dd174c1c442004d770ab90b57f8e
beanbeanjuice <beanbeanjuice@outlook.com>
2021-05-11 14:40:48 -0400

    Updated the SQL Code

--- sql/tables/guild_information.sql
+++ sql/tables/guild_information.sql
@@ -5,4 +5,5 @@ moderator_role_id BIGINT DEFAULT 0,
 twitch_channel_id BIGINT DEFAULT 0,
 muted_role_id BIGINT DEFAULT 0,
 live_notifications_role_id BIGINT DEFAULT 0,
-notify_on_update TINYINT DEFAULT 1);
+notify_on_update TINYINT DEFAULT 1,
+bot_update_channel_id BIGINT DEFAULT 0);
