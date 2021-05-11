c88e3d7bd2963a60dbf68fcadeedac2191700ab4
beanbeanjuice <beanbeanjuice@outlook.com>
2021-05-11 04:14:11 -0400

    Updated the SQL Files for the guild_information

--- sql/tables/guild_information.sql
+++ sql/tables/guild_information.sql
@@ -1,2 +1,2 @@
 CREATE TABLE guild_information
-(guild_id BIGINT, prefix TEXT, moderator_role_id BIGINT, twitch_channel_id BIGINT, muted_role_id BIGINT);
+(guild_id BIGINT, prefix TEXT, moderator_role_id BIGINT, twitch_channel_id BIGINT, muted_role_id BIGINT, live_notifications_role_id BIGINT);
