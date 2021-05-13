4a0523a3dcd2baef93c83b0afdda723f35eda9a3
beanbeanjuice <beanbeanjuice@outlook.com>
2021-05-12 21:12:08 -0400

    Updated Javadocs

--- src/main/java/com/beanbeanjuice/utility/cafe/CafeCustomer.java
+++ src/main/java/com/beanbeanjuice/utility/cafe/CafeCustomer.java
@@ -5,28 +5,48 @@ import org.jetbrains.annotations.Nullable;
 
 import java.sql.Timestamp;
 
+/**
+ * A custom class for {@link CafeCustomer}.
+ *
+ * @author beanbeanjuice
+ */
 public class CafeCustomer {
 
     private String userID;
     private double beanCoinAmount;
     private Timestamp lastServingTime;
 
+    /**
+     * Creates a new {@link CafeCustomer} object.
+     * @param userID The Discord ID of the {@link net.dv8tion.jda.api.entities.User User}.
+     * @param beanCoinAmount The amount of money in the specified {@link CafeCustomer}'s bank account.
+     * @param lastServingTime The {@link Timestamp} of the last serving time.
+     */
     public CafeCustomer(@NotNull String userID, @NotNull Double beanCoinAmount, @NotNull Timestamp lastServingTime) {
         this.userID = userID;
         this.beanCoinAmount = beanCoinAmount;
         this.lastServingTime = lastServingTime;
     }
 
+    /**
+     * @return The Discord user ID of the {@link CafeCustomer}.
+     */
     @NotNull
     public String getUserID() {
         return userID;
     }
 
+    /**
+     * @return The amount of money in the specified {@link CafeCustomer}'s bank account.
+     */
     @NotNull
     public Double getBeanCoinAmount() {
         return beanCoinAmount;
     }
 
+    /**
+     * @return The time of the last serving for the {@link CafeCustomer}.
+     */
     @Nullable
     public Timestamp getLastServingTime() {
         return lastServingTime;
