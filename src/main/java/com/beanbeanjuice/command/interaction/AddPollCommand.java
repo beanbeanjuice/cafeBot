d60eb81ee79ceb1b4ec841069158ae14b4cc0c3b
beanbeanjuice <beanbeanjuice@outlook.com>
2021-05-14 13:35:02 -0400

    Cleaned Up Code in the Poll Command

--- src/main/java/com/beanbeanjuice/command/interaction/AddPollCommand.java
+++ src/main/java/com/beanbeanjuice/command/interaction/AddPollCommand.java
@@ -30,10 +30,12 @@ public class AddPollCommand implements ICommand {
     @Override
     public void handle(CommandContext ctx, ArrayList<String> args, User user, GuildMessageReceivedEvent event) {
 
+        // Checking for if they are a moderator.
         if (!BeanBot.getGeneralHelper().isModerator(event.getMember(), event.getGuild(), event)) {
             return;
         }
 
+        // Makes sure that guilds only have 3 polls.
         if (BeanBot.getPollHandler().getPollsForGuild(event.getGuild()).size()+1 > 3) {
             event.getChannel().sendMessage(BeanBot.getGeneralHelper().errorEmbed(
                     "Too Many Poll",
@@ -43,11 +45,12 @@ public class AddPollCommand implements ICommand {
             return;
         }
 
-        String title = removeUnderscores(args.get(0));
-        String description = removeUnderscores(args.get(1));
+        String title = BeanBot.getGeneralHelper().removeUnderscores(args.get(0));
+        String description = BeanBot.getGeneralHelper().removeUnderscores(args.get(1));
         Integer minutes = Integer.parseInt(args.get(2));
         ArrayList<String> arguments = convertToList(args.get(3));
 
+        // Making sure the poll channel exists.
         TextChannel pollChannel = BeanBot.getGuildHandler().getCustomGuild(event.getGuild()).getPollChannel();
         if (pollChannel == null) {
             event.getChannel().sendMessage(BeanBot.getGeneralHelper().errorEmbed(
@@ -57,6 +60,7 @@ public class AddPollCommand implements ICommand {
             return;
         }
 
+        // Making sure there are less than 20 arguments.
         if (arguments.size() > 20) {
             pollChannel.sendMessage(BeanBot.getGeneralHelper().errorEmbed(
                     "Too Many Items",
@@ -67,6 +71,7 @@ public class AddPollCommand implements ICommand {
 
         Timestamp timestamp = new Timestamp(System.currentTimeMillis() + (minutes*60000));
 
+        // Sending a message in the poll channel.
         pollChannel.sendMessage(startingPollEmbed()).queue(e -> {
             Poll poll = new Poll(event.getGuild().getId(), e.getId(), timestamp);
 
@@ -75,15 +80,13 @@ public class AddPollCommand implements ICommand {
                 return;
             }
 
-            e.editMessage(pollEmbed(title, description, minutes.toString(), arguments)).queue();
+            e.editMessage(pollEmbed(title, description, minutes, arguments)).queue();
 
             ArrayList<PollEmoji> pollEmojis = new ArrayList<>(Arrays.asList(PollEmoji.values()));
-
             for (int i = 0; i < arguments.size(); i++) {
                 e.addReaction(pollEmojis.get(i).getUnicode()).queue();
             }
         });
-
     }
 
     @NotNull
@@ -97,12 +100,12 @@ public class AddPollCommand implements ICommand {
 
     @NotNull
     private MessageEmbed pollEmbed(@NotNull String pollTitle, @NotNull String pollDescription,
-                                   @NotNull String pollTime, @NotNull ArrayList<String> arguments) {
+                                   @NotNull Integer pollTime, @NotNull ArrayList<String> arguments) {
         EmbedBuilder embedBuilder = new EmbedBuilder();
         embedBuilder.setAuthor(pollTitle);
+        embedBuilder.setColor(BeanBot.getGeneralHelper().getRandomColor());
 
         ArrayList<PollEmoji> pollEmojis = new ArrayList<>(Arrays.asList(PollEmoji.values()));
-
         StringBuilder stringBuilder = new StringBuilder();
 
         for (int i = 0; i < arguments.size(); i++) {
@@ -111,27 +114,22 @@ public class AddPollCommand implements ICommand {
         }
 
         stringBuilder.append("\n\n").append(pollDescription);
-
         embedBuilder.setDescription(stringBuilder.toString());
-        embedBuilder.setFooter("This poll will end in " + pollTime + " minutes from when it was created.");
-        embedBuilder.setColor(BeanBot.getGeneralHelper().getRandomColor());
+
+        if (pollTime == 1) {
+            embedBuilder.setFooter("This poll will end in " + pollTime + " minute from when it was created.");
+        } else {
+            embedBuilder.setFooter("This poll will end in " + pollTime + " minutes from when it was created.");
+        }
         return embedBuilder.build();
     }
 
     @NotNull
-    private String removeUnderscores(@NotNull String string) {
-        return string.replaceAll("_", " ");
-    }
-
-    @NotNull
     private ArrayList<String> convertToList(@NotNull String string) {
-
         ArrayList<String> arrayList = new ArrayList<>();
-
         for (String argument : string.split(",")) {
-            arrayList.add(removeUnderscores(argument));
+            arrayList.add(BeanBot.getGeneralHelper().removeUnderscores(argument));
         }
-
         return arrayList;
     }
 
