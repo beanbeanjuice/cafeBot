ea866943ed9118bf5b66be0b8b4e22e79e4c21a5
beanbeanjuice <beanbeanjuice@outlook.com>
2021-05-14 18:49:42 -0400

    Cleaned Up Code
    The raffle branch is complete.

--- src/main/java/com/beanbeanjuice/utility/raffle/RaffleHandler.java
+++ src/main/java/com/beanbeanjuice/utility/raffle/RaffleHandler.java
@@ -13,13 +13,20 @@ import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
 
-
+/**
+ * A class used for handling {@link Raffle} objects.
+ *
+ * @author beanbeanjuice
+ */
 public class RaffleHandler {
 
     private HashMap<String, ArrayList<Raffle>> raffles;
     private Timer raffleTimer;
     private TimerTask raffleTimerTask;
 
+    /**
+     * Create a new {@link RaffleHandler} object.
+     */
     public RaffleHandler() {
         raffles = new HashMap<>();
         getRafflesFromDatabase();
@@ -37,13 +44,10 @@ public class RaffleHandler {
             public void run() {
 
                 raffles.forEach((key, value) -> {
-
                     for (Raffle raffle : value) {
 
                         // Checking if it SHOULD be checked.
                         if (raffle.isFinished()) {
-
-                            System.out.println("Checking Raffle");
 
                             // Checking if the PollChannel is Null
                             TextChannel raffleChannel = BeanBot.getGuildHandler().getCustomGuild(key).getRaffleChannel();
@@ -57,77 +61,65 @@ public class RaffleHandler {
                                 raffleChannel.retrieveMessageById(raffle.getMessageID()).queue((message) -> {
                                     // Edit Message If Not Null
                                     // Get the reactions
-                                    MessageReaction reaction = new ArrayList<>(message.getReactions()).get(0);
-
                                     ArrayList<User> potentialUsers = new ArrayList<>();
 
-                                    for (MessageReaction reactionMessage : message.getReactions()) {
-                                        reactionMessage.retrieveUsers().queue(users -> {
-                                            for (User user : users) {
-                                                System.out.println("User: " + user.getName());
-                                                if (!user.isBot() && !potentialUsers.contains(user)) {
-                                                    System.out.println("Allowed User: " + user.getName());
-                                                    potentialUsers.add(user);
-                                                }
+                                    message.getReactions().get(0).retrieveUsers().queue(users -> {
+                                        for (User user : users) {
+                                            if (!user.isBot() && !potentialUsers.contains(user)) {
+                                                potentialUsers.add(user);
                                             }
+                                        }
 
-                                            System.out.println("Users Size: " + potentialUsers.size());
-                                            System.out.println("Message ID: " + message.getId());
+                                        ArrayList<User> winners = new ArrayList<>();
 
-                                            ArrayList<User> winners = new ArrayList<>();
-
-                                            if (potentialUsers.size() > raffle.getWinnerAmount()) {
-                                                for (int i = 0; i < raffle.getWinnerAmount(); i++) {
-                                                    System.out.println("Getting Winner #" + i);
-                                                    User user = potentialUsers.get(BeanBot.getGeneralHelper().getRandomNumber(0, (potentialUsers.size() - 1)));
-
-                                                    if (winners.contains(user)) {
-                                                        User newUser = potentialUsers.get(BeanBot.getGeneralHelper().getRandomNumber(0, (potentialUsers.size() - 1)));
-                                                        while (newUser == user) {
-                                                            newUser = potentialUsers.get(BeanBot.getGeneralHelper().getRandomNumber(0, (potentialUsers.size() - 1)));
-                                                        }
-                                                        user = newUser;
+                                        if (potentialUsers.size() > raffle.getWinnerAmount()) {
+                                            for (int i = 0; i < raffle.getWinnerAmount(); i++) {
+                                                User user = potentialUsers.get(BeanBot.getGeneralHelper().getRandomNumber(0, (potentialUsers.size() - 1)));
+                                                if (winners.contains(user)) {
+                                                    User newUser = potentialUsers.get(BeanBot.getGeneralHelper().getRandomNumber(0, (potentialUsers.size() - 1)));
+                                                    while (newUser == user) {
+                                                        newUser = potentialUsers.get(BeanBot.getGeneralHelper().getRandomNumber(0, (potentialUsers.size() - 1)));
                                                     }
-                                                    winners.add(user);
+                                                    user = newUser;
                                                 }
-                                            } else {
-                                                winners = potentialUsers;
+                                                winners.add(user);
                                             }
+                                        } else {
+                                            winners = potentialUsers;
+                                        }
 
-                                            String title = message.getEmbeds().get(0).getAuthor().getName();
-                                            String description = message.getEmbeds().get(0).getFields().get(0).getValue();
+                                        String title = message.getEmbeds().get(0).getAuthor().getName();
+                                        String description = message.getEmbeds().get(0).getFields().get(0).getValue();
 
-                                            message.editMessage(winnerEmbed(title, description, winners)).queue();
+                                        message.editMessage(winnerEmbed(title, description, winners)).queue();
 
-                                            // Remove it
-                                            removeRaffleFromDatabase(raffle);
-                                            value.remove(raffle);
-
-                                        });
-                                    }
-
+                                        // Remove it
+                                        removeRaffleFromDatabase(raffle);
+                                        value.remove(raffle);
+                                    });
                                 }, (failure) -> {
 
                                     // This means the message does not exist.
                                     removeRaffleFromDatabase(raffle);
                                     value.remove(raffle);
                                 });
-
                             }
-
-                        } else {
-                            System.out.println("Ignoring Raffle");
                         }
-
                     }
-
                 });
-
             }
         };
-        raffleTimer.scheduleAtFixedRate(raffleTimerTask, 0, 1000);
+        raffleTimer.scheduleAtFixedRate(raffleTimerTask, 0, 30000);
     }
 
+    /**
+     * The winner {@link MessageEmbed} for the {@link RaffleHandler}.
+     * @param title The title of the {@link MessageEmbed}.
+     * @param description The description of the {@link MessageEmbed}.
+     * @param winners The {@link ArrayList<User> winners} of the {@link Raffle}.
+     * @return The completed {@link MessageEmbed}.
+     */
+    @NotNull
     private MessageEmbed winnerEmbed(@NotNull String title, @NotNull String description, @NotNull ArrayList<User> winners) {
         EmbedBuilder embedBuilder = new EmbedBuilder();
         embedBuilder.setAuthor(title);
@@ -142,7 +134,6 @@ public class RaffleHandler {
             StringBuilder winnerBuilder = new StringBuilder();
             for (int i = 0; i < winners.size(); i++) {
                 winnerBuilder.append(winners.get(i).getAsMention());
-
                 if (i != winners.size() - 1) {
                     winnerBuilder.append(", ");
                 }
@@ -152,8 +143,13 @@ public class RaffleHandler {
         return embedBuilder.build();
     }
 
+    /**
+     * Removes a {@link Raffle} from the database.
+     * @param raffle The {@link Raffle} to be removed.
+     * @return Whether or not the removal was successful.
+     */
+    @NotNull
     private Boolean removeRaffleFromDatabase(@NotNull Raffle raffle) {
-
         Connection connection = BeanBot.getSQLServer().getConnection();
         String arguments = "DELETE FROM beanbot.raffles WHERE message_id = (?) and guild_id = (?);";
 
@@ -161,19 +157,21 @@ public class RaffleHandler {
             PreparedStatement statement = connection.prepareStatement(arguments);
             statement.setLong(1, Long.parseLong(raffle.getMessageID()));
             statement.setLong(2, Long.parseLong(raffle.getGuildID()));
-
             statement.execute();
             return true;
         } catch (SQLException e) {
             BeanBot.getLogManager().log(this.getClass(), LogLevel.WARN, "Error Removing Raffle: " + e.getMessage());
             return false;
         }
-
     }
 
+    /**
+     * Add a {@link Raffle} to the database.
+     * @param raffle The {@link Raffle} to add.
+     * @return Whether or not adding the {@link Raffle} was successful.
+     */
     @NotNull
     public Boolean addRaffle(@NotNull Raffle raffle) {
-
         Connection connection = BeanBot.getSQLServer().getConnection();
         String arguments = "INSERT INTO beanbot.raffles (guild_id, message_id, ending_time, winner_amount) VALUES (?,?,?,?);";
 
@@ -183,7 +181,6 @@ public class RaffleHandler {
             statement.setLong(2, Long.parseLong(raffle.getMessageID()));
             statement.setTimestamp(3, raffle.getRaffleEndTime());
             statement.setInt(4, raffle.getWinnerAmount());
-
             statement.execute();
         } catch (SQLException e) {
             BeanBot.getLogManager().log(this.getClass(), LogLevel.WARN, "Error Adding Raffle: " + e.getMessage());
@@ -196,9 +193,13 @@ public class RaffleHandler {
 
         raffles.get(raffle.getGuildID()).add(raffle);
         return true;
-
     }
 
+    /**
+     * Get the {@link ArrayList<Raffle>} for a specified {@link Guild}.
+     * @param guildID The ID of the {@link Guild}.
+     * @return The {@link ArrayList<Raffle>} for the {@link Guild}.
+     */
     @NotNull
     public ArrayList<Raffle> getRafflesForGuild(@NotNull String guildID) {
         if (raffles.get(guildID) != null) {
@@ -207,11 +208,19 @@ public class RaffleHandler {
         return new ArrayList<>();
     }
 
+    /**
+     * Get the {@link ArrayList<Raffle>} for a specified {@link Guild}.
+     * @param guild The {@link Guild}.
+     * @return The {@link ArrayList<Raffle>} for the {@link Guild}.
+     */
     @NotNull
     public ArrayList<Raffle> getRafflesForGuild(@NotNull Guild guild) {
         return getRafflesForGuild(guild.getId());
     }
 
+    /**
+     * Refresh the {@link Raffle} cache.
+     */
     private void getRafflesFromDatabase() {
         Connection connection = BeanBot.getSQLServer().getConnection();
         String arguments = "SELECT * FROM beanbot.raffles;";
@@ -219,9 +228,7 @@ public class RaffleHandler {
         try {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(arguments);
-
             while (resultSet.next()) {
-
                 String guildID = String.valueOf(resultSet.getLong(1));
                 String messageID = String.valueOf(resultSet.getLong(2));
                 Timestamp raffleEndTime = resultSet.getTimestamp(3);
@@ -232,7 +239,6 @@ public class RaffleHandler {
                 }
 
                 raffles.get(guildID).add(new Raffle(guildID, messageID, raffleEndTime, winnerAmount));
-
             }
         } catch (SQLException e) {
             BeanBot.getLogManager().log(this.getClass(), LogLevel.WARN, "Error Retrieving Raffles: " + e.getMessage());
