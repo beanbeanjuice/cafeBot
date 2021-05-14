228a5533f7bb465d15785a9d34e921170d9099be
beanbeanjuice <beanbeanjuice@outlook.com>
2021-05-13 22:36:20 -0400

    Added Updating the Poll Channel

--- src/main/java/com/beanbeanjuice/utility/poll/PollHandler.java
+++ src/main/java/com/beanbeanjuice/utility/poll/PollHandler.java
@@ -1,14 +1,19 @@
 package com.beanbeanjuice.utility.poll;
 
 import com.beanbeanjuice.main.BeanBot;
+import com.beanbeanjuice.utility.logger.LogLevel;
+import net.dv8tion.jda.api.entities.Guild;
+import org.jetbrains.annotations.NotNull;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * A class used for handling {@link Poll}s.
+ *
+ * @author beanbeanjuice
+ */
 public class PollHandler {
 
     private HashMap<String, ArrayList<Poll>> activePolls;
@@ -42,6 +47,64 @@ public class PollHandler {
 
         activePolls.get(guildID).add(poll);
         return true;
+    }
+
+    private void getPollsFromDatabase() {
+
+        Connection connection = BeanBot.getSQLServer().getConnection();
+        String arguments = "SELECT * FROM beanbot.polls;";
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(arguments);
+
+            while (resultSet.next()) {
+                String guildID = String.valueOf(resultSet.getLong(1));
+                String messageID = String.valueOf(resultSet.getLong(2));
+                Timestamp pollEndTime = resultSet.getTimestamp(3);
+
+                if (activePolls.get(guildID) == null) {
+                    activePolls.put(guildID, new ArrayList<>());
+                }
+
+                activePolls.get(guildID).add(new Poll(guildID, messageID, pollEndTime));
+            }
+        } catch (SQLException e) {
+            BeanBot.getLogManager().log(this.getClass(), LogLevel.WARN, "Error Retrieving Polls from Database: " + e.getMessage());
+        }
+
+    }
+
+    @NotNull
+    private Boolean removePollFromDatabase(@NotNull Poll poll) {
+
+        Connection connection = BeanBot.getSQLServer().getConnection();
+        String arguments = "DELETE FROM beanbot.polls WHERE guild_id = (?) AND message_id = (?);";
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(arguments);
+            statement.setLong(1, Long.parseLong(poll.getGuildID()));
+            statement.setLong(2, Long.parseLong(poll.getMessageID()));
+            statement.execute();
+            return true;
+        } catch (SQLException e) {
+            BeanBot.getLogManager().log(this.getClass(), LogLevel.WARN, "Error Removing Poll from Database: " + e.getMessage());
+            return false;
+        }
+
+    }
+
+    @NotNull
+    public ArrayList<Poll> getPollsForGuild(@NotNull String guildID) {
+        if (activePolls.get(guildID) != null) {
+            return activePolls.get(guildID);
+        }
+        return new ArrayList<>();
+    }
+
+    @NotNull
+    public ArrayList<Poll> getPollsForGuild(@NotNull Guild guild) {
+        return getPollsForGuild(guild.getId());
     }
 
 
