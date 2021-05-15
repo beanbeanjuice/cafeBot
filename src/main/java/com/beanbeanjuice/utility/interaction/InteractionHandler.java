659b341f0f52f10e8d82fe69a688e9df87ecab65
beanbeanjuice <beanbeanjuice@outlook.com>
2021-05-15 19:24:40 -0400

    Finished the InteractionHandler

---
+++ src/main/java/com/beanbeanjuice/utility/interaction/InteractionHandler.java
@@ -0,0 +1,143 @@
+package com.beanbeanjuice.utility.interaction;
+
+import com.beanbeanjuice.main.CafeBot;
+
+import java.util.ArrayList;
+
+/**
+ * A custom class used for Interaction Commands
+ *
+ * @author beanbeanjuice
+ */
+public class InteractionHandler {
+
+    public String getHugImage() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("https://i.pinimg.com/originals/f2/80/5f/f2805f274471676c96aff2bc9fbedd70.gif");
+        arrayList.add("https://media.tenor.com/images/b6d0903e0d54e05bb993f2eb78b39778/tenor.gif");
+        arrayList.add("https://64.media.tumblr.com/tumblr_ma7l17EWnk1rq65rlo1_500.gif");
+        arrayList.add("https://i.pinimg.com/originals/85/72/a1/8572a1d1ebaa45fae290e6760b59caac.gif");
+        arrayList.add("https://i.imgur.com/XEs1SWQ.gif");
+        arrayList.add("https://acegif.com/wp-content/gif/anime-hug-38.gif");
+        arrayList.add("https://64.media.tumblr.com/f2a878657add13aa09a5e089378ec43d/tumblr_n5uovjOi931tp7433o1_500.gif");
+        arrayList.add("https://acegif.com/wp-content/gif/anime-hug-30.gif");
+        arrayList.add("https://tenor.com/view/mochi-peachcat-mochi-peachcat-hug-pat-gif-19092449");
+        arrayList.add("https://tenor.com/view/milk-and-mocha-hug-cute-kawaii-love-gif-12535134");
+        arrayList.add("https://tenor.com/view/milk-and-mocha-bear-couple-line-hug-cant-breathe-gif-12687187");
+        arrayList.add("https://i.pinimg.com/originals/6d/b5/4c/6db54c4d6dad5f1f2863d878cfb2d8df.gif");
+        arrayList.add("https://64.media.tumblr.com/tumblr_ma7l17EWnk1rq65rlo1_500.gif");
+        arrayList.add("https://i.imgur.com/pQHEJWZ.gif");
+        return arrayList.get(CafeBot.getGeneralHelper().getRandomNumber(0, arrayList.size()-1));
+    }
+
+    public String getPunchImage() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("https://media1.tenor.com/images/31686440e805309d34e94219e4bedac1/tenor.gif?itemid=4790446");
+        arrayList.add("https://media.tenor.com/images/8a79543998d6878be573aab94ae86456/tenor.gif");
+        arrayList.add("https://i.pinimg.com/originals/d4/17/ce/d417ce25489868ae4bf177de73308aa5.gif");
+        arrayList.add("https://thumbs.gfycat.com/AmazingAngryCoelacanth-max-1mb.gif");
+        arrayList.add("https://media.tenor.com/images/f7b498a905f3e8c964ad5d97bf176e1f/tenor.gif");
+        arrayList.add("https://64.media.tumblr.com/01622a00635a61442a5548e70cd4aff8/a55a2448b1c12be9-0c/s640x960/298f2338ed0eb578a8da10003e4427a3a004a237.gifv");
+        arrayList.add("https://qph.fs.quoracdn.net/main-qimg-024b359517a272d10b26f703c37e5c28");
+        arrayList.add("https://tenor.com/view/milk-so-cute-punch-gif-15151443");
+        arrayList.add("https://media.tenor.com/images/9ff746e22fbb39ebf28f431c158f769f/tenor.gif");
+        return arrayList.get(CafeBot.getGeneralHelper().getRandomNumber(0, arrayList.size()-1));
+    }
+
+    public String getKissImage() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("https://media0.giphy.com/media/FqBTvSNjNzeZG/source.gif");
+        arrayList.add("https://i.imgur.com/OE7lSSY.gif");
+        arrayList.add("https://media2.giphy.com/media/bGm9FuBCGg4SY/giphy.gif");
+        arrayList.add("https://i.pinimg.com/originals/f7/e8/a4/f7e8a4abac5d9e64784bd97480863a19.gif");
+        arrayList.add("https://i.pinimg.com/originals/8c/ab/4f/8cab4f4c73547d077c56066461c40a5e.gif");
+        arrayList.add("https://tenor.com/view/milk-and-mocha-cute-kiss-bear-gif-12873719");
+        arrayList.add("https://tenor.com/view/milk-and-mocha-kiss-hey-cute-gif-14589013");
+        arrayList.add("https://tenor.com/view/cat-love-kisses-nose-kiss-gif-17947049");
+        return arrayList.get(CafeBot.getGeneralHelper().getRandomNumber(0, arrayList.size()-1));
+    }
+
+    public String getTackleImage() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("https://i.imgur.com/GE1psc2.gif");
+        arrayList.add("https://media.tenor.com/images/51e2ba00b6246dbfbbd97a2f38c8307c/tenor.gif");
+        arrayList.add("https://media.tenor.com/images/2898c78849535bb7301c9ac2ce69979a/tenor.gif");
+        arrayList.add("https://tenor.com/view/milk-and-mocha-bear-yay-happy-excited-hug-gif-16976981");
+        arrayList.add("https://media.tenor.com/images/0dfd068968cd55331cf27d20e789bcee/tenor.gif");
+        arrayList.add("https://media1.tenor.com/images/d8ed2bfd23edcf0fc40ec4472f6bad38/tenor.gif?itemid=5152435");
+        arrayList.add("https://media.tenor.com/images/ce660291ed80954b368597d4b4d76677/tenor.gif");
+        return arrayList.get(CafeBot.getGeneralHelper().getRandomNumber(0, arrayList.size()-1));
+    }
+
+    public String getSlapImage() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("https://media0.giphy.com/media/Zau0yrl17uzdK/giphy.gif");
+        arrayList.add("https://i.pinimg.com/originals/fc/e1/2d/fce12d3716f05d56549cc5e05eed5a50.gif");
+        arrayList.add("https://i.pinimg.com/originals/4e/9e/a1/4e9ea150354ad3159339b202cbc6cad9.gif");
+        arrayList.add("https://media.tenor.com/images/9e7a8a7473f6535081805f0e85b7a09f/tenor.gif");
+        arrayList.add("https://media1.tenor.com/images/b6d8a83eb652a30b95e87cf96a21e007/tenor.gif?itemid=10426943");
+        arrayList.add("https://i.pinimg.com/originals/fe/39/cf/fe39cfc3be04e3cbd7ffdcabb2e1837b.gif");
+        arrayList.add("https://i.imgur.com/fm49srQ.gif");
+        arrayList.add("https://i.pinimg.com/originals/65/57/f6/6557f684d6ffcd3cd4558f695c6d8956.gif");
+        arrayList.add("https://i.imgur.com/o2SJYUS.gif");
+        return arrayList.get(CafeBot.getGeneralHelper().getRandomNumber(0, arrayList.size()-1));
+    }
+
+    public String getStabImage() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("https://i0.wp.com/blackandyellowotakugamers.com/wp-content/uploads/2017/06/ayase-stab.gif?resize=640%2C360&ssl=1");
+        arrayList.add("https://lh4.googleusercontent.com/proxy/rJcn_Dbvigo9RcC7cG2roccmEFsn2bjHahIjWrcnXQopZN83m0K7GSDC1vRuxvxUrs91vgJjhai6ttfeB8s2JjZ0BgNucrLVaN9u3owHNS-T=s0-d");
+        arrayList.add("https://i.gifer.com/EJEW.gif");
+        arrayList.add("https://images6.fanpop.com/image/photos/38400000/Anime-stabbing-the-floor-anime-38465649-500-376.gif");
+        return arrayList.get(CafeBot.getGeneralHelper().getRandomNumber(0, arrayList.size()-1));
+    }
+
+    public String getHmphImage() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("https://i.pinimg.com/originals/15/a0/52/15a05247ac88d901524adde074651bb6.gif");
+        arrayList.add("https://i.pinimg.com/originals/b7/e1/32/b7e132fd3f4e110ea54ef8aa8f4eebbe.gif");
+        arrayList.add("https://media.tenor.com/images/bae7c01893337837f652ef4932225bb3/tenor.gif");
+        arrayList.add("https://image.myanimelist.net/ui/D7ahedOlctZp9mcCPCIwK_Ecb3sRiVT2GZ6rB8qrAL2ic1o1A2V6r2f6t1ptoq0MAS2D_e7xg-yS3K4Rrlu-_vnKZO0LjfyjIJ61jzQErjuWrt3R2lud0fbmwAO4owMv");
+        arrayList.add("https://thumbs.gfycat.com/ThornySizzlingAtlanticsharpnosepuffer-max-1mb.gif");
+        arrayList.add("https://i.imgur.com/UILjVng.gif");
+        return arrayList.get(CafeBot.getGeneralHelper().getRandomNumber(0, arrayList.size()-1));
+    }
+
+    public String getPoutImage() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("https://i.imgur.com/Z3Hdtos.gif");
+        arrayList.add("https://media1.tenor.com/images/1e3dd3aaafa7481aa7f47e0157fc91c9/tenor.gif?itemid=12390498");
+        arrayList.add("https://i.pinimg.com/originals/e5/6e/1a/e56e1ae197ea11668756e6e82407e5c5.gif");
+        arrayList.add("https://media.tenor.com/images/fb76ab83c98779c3dae8dc1afef7b5df/tenor.gif");
+        arrayList.add("https://i.pinimg.com/originals/e8/a3/9c/e8a39c41b8e2eacdf0fd4b5889a5f323.gif");
+        arrayList.add("https://media2.giphy.com/media/X3VrxPijowGC4/giphy.gif");
+        arrayList.add("https://cutewallpaper.org/21/anime-pout-face/Moms-Pout-GIF-by-Shumatsu.gif");
+        return arrayList.get(CafeBot.getGeneralHelper().getRandomNumber(0, arrayList.size()-1));
+    }
+
+    public String getThrowImage() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("https://i.pinimg.com/originals/8d/ab/29/8dab296aed2cbe25af8ebb4703517356.gif");
+        arrayList.add("https://media1.tenor.com/images/9ff361ceb7cb526cd7874d10095dfd76/tenor.gif?itemid=16842604");
+        arrayList.add("https://media1.tenor.com/images/189e189c645be7debf05bc17026cd915/tenor.gif?itemid=16221358");
+        arrayList.add("https://image.myanimelist.net/ui/q-babdSqGwiJF-zQZJJ0ecZzOC34ttAGza624-gyjeV9rLaBHOyfSoLcrPA8RpZuyM35RqFzOcnaeZZIU0G4GDQxeStT_B6QcDPdAKG2OT8");
+        arrayList.add("https://i.pinimg.com/originals/56/55/d3/5655d367d927ec5920f26c143c50072c.gif");
+        arrayList.add("https://thumbs.gfycat.com/LargeScratchyFinch-size_restricted.gif");
+        return arrayList.get(CafeBot.getGeneralHelper().getRandomNumber(0, arrayList.size()-1));
+    }
+
+    public String getSmileImage() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("https://pa1.narvii.com/6314/69057088d88cccf7cce70f19192e112994cc055b_hq.gif");
+        arrayList.add("https://i.pinimg.com/originals/82/b3/9c/82b39c323ca376e9bb5844a54973fc42.gif");
+        arrayList.add("https://66.media.tumblr.com/7216949e7f3662b24c28f3d700c9f096/tumblr_plfhtpnwzP1th206io1_400.gif");
+        arrayList.add("https://media.tenor.com/images/7a15e5fa64b4b2b4dde8554339d3cfa0/tenor.gif");
+        arrayList.add("https://64.media.tumblr.com/tumblr_m8wm9nCUGf1rrftqho1_500.gif");
+        arrayList.add("https://lh6.googleusercontent.com/proxy/T9bIVI68CLYgQzxxAhu6yHB1VQrWtsqAQimkAV6PkqG3UG8-Xnq0Hbud3aTS8vq68dsszY1A1wQUOcxxvjly7wDDJAq_qQwcITbDG4UDEnp98X4=s0-d");
+        arrayList.add("https://64.media.tumblr.com/4107c390b3cb3b8a215bcd4c5f1b1dc3/tumblr_o3x6o2REG91tydz8to1_540.gifv");
+        arrayList.add("https://i.pinimg.com/originals/98/11/96/981196752db77080887150e27b18c70e.gif");
+        arrayList.add("https://64.media.tumblr.com/245e1dd0a0522ce16a011878726896e4/tumblr_inline_nl5noj41HV1sptc1c.gif");
+        return arrayList.get(CafeBot.getGeneralHelper().getRandomNumber(0, arrayList.size()-1));
+    }
+
+}
