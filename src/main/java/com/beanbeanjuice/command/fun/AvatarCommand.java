cf831eaf68035275c48035204060a0155d5d0414
beanbeanjuice <beanbeanjuice@outlook.com>
2021-05-15 18:39:52 -0400

    Moved Commands
    Moved the `poll`, `raffle`, and `avatar` command to the `FUN` category.

--- src/main/java/com/beanbeanjuice/command/fun/AvatarCommand.java
+++ src/main/java/com/beanbeanjuice/command/fun/AvatarCommand.java
@@ -1,4 +1,4 @@
-package com.beanbeanjuice.command.interaction;
+package com.beanbeanjuice.command.fun;
 
 import com.beanbeanjuice.main.CafeBot;
 import com.beanbeanjuice.utility.command.CommandContext;
@@ -72,6 +72,6 @@ public class AvatarCommand implements ICommand {
 
     @Override
     public CategoryType getCategoryType() {
-        return CategoryType.INTERACTION;
+        return CategoryType.FUN;
     }
 }
