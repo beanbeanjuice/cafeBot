34c6c24ab228c206858d8611196429ef41b6a28f
beanbeanjuice <beanbeanjuice@outlook.com>
2021-05-16 07:41:23 -0400

    Updated the Hug Command

--- src/main/java/com/beanbeanjuice/command/interaction/HugCommand.java
+++ src/main/java/com/beanbeanjuice/command/interaction/HugCommand.java
@@ -24,41 +24,42 @@ public class HugCommand implements ICommand {
     @Override
     public void handle(CommandContext ctx, ArrayList<String> args, User user, GuildMessageReceivedEvent event) {
         String url = CafeBot.getInteractionHandler().getHugImage();
+        String hugger = user.getName();
+        String huggee = CafeBot.getGeneralHelper().getUser(args.get(0)).getName();
+        String message = "**" + huggee + "**, you have been hugged by **" + hugger + "**!";
+
         if (args.size() == 1) {
-            event.getChannel().sendMessage(hugEmbed(user, CafeBot.getGeneralHelper().getUser(args.get(0)), url)).queue();
+            event.getChannel().sendMessage(message).embed(hugEmbed(url)).queue();
         } else {
-            event.getChannel().sendMessage(hugWithDescriptionEmbed(user, CafeBot.getGeneralHelper().getUser(args.get(0)), url, args)).queue();
+            event.getChannel().sendMessage(message).embed(hugWithDescriptionEmbed(url, args)).queue();
         }
     }
 
     @NotNull
-    private MessageEmbed hugEmbed(@NotNull User hugger, @NotNull User huggee, @NotNull String link) {
+    private MessageEmbed hugEmbed(@NotNull String link) {
         EmbedBuilder embedBuilder = new EmbedBuilder();
         embedBuilder.setImage(link);
         embedBuilder.setColor(CafeBot.getGeneralHelper().getRandomColor());
-        embedBuilder.setDescription(hugger.getAsMention() + " hugs " + huggee.getAsMention() + ".");
         return embedBuilder.build();
     }
 
     @NotNull
-    private MessageEmbed hugWithDescriptionEmbed(@NotNull User hugger, @NotNull User huggee, @NotNull String link, @NotNull ArrayList<String> args) {
+    private MessageEmbed hugWithDescriptionEmbed(@NotNull String link, @NotNull ArrayList<String> args) {
         EmbedBuilder embedBuilder = new EmbedBuilder();
         embedBuilder.setImage(link);
         embedBuilder.setColor(CafeBot.getGeneralHelper().getRandomColor());
 
         StringBuilder descriptionBuilder = new StringBuilder();
-        descriptionBuilder.append(hugger.getAsMention()).append(" hugs ").append(huggee.getAsMention()).append(".\n\n")
-                .append("*");
+        descriptionBuilder.append("\"");
 
         for (int i = 1; i < args.size(); i++) {
             descriptionBuilder.append(args.get(i));
-
             if (i != args.size() - 1) {
                 descriptionBuilder.append(" ");
             }
         }
 
-        descriptionBuilder.append("*");
+        descriptionBuilder.append("\"");
         embedBuilder.setDescription(descriptionBuilder.toString());
         return embedBuilder.build();
     }
