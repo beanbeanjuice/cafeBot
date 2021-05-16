5ea63e3ecaa14e99598d9f845ed78e7a45c98b3f
beanbeanjuice <beanbeanjuice@outlook.com>
2021-05-16 08:43:10 -0400

    Created the Kiss Command

---
+++ src/main/java/com/beanbeanjuice/command/interaction/KissCommand.java
@@ -0,0 +1,100 @@
+package com.beanbeanjuice.command.interaction;
+
+import com.beanbeanjuice.main.CafeBot;
+import com.beanbeanjuice.utility.command.CommandContext;
+import com.beanbeanjuice.utility.command.ICommand;
+import com.beanbeanjuice.utility.command.usage.Usage;
+import com.beanbeanjuice.utility.command.usage.categories.CategoryType;
+import com.beanbeanjuice.utility.command.usage.types.CommandType;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+/**
+ * A command used to kiss people.
+ *
+ * @author beanbeanjuice
+ */
+public class KissCommand implements ICommand {
+
+    @Override
+    public void handle(CommandContext ctx, ArrayList<String> args, User user, GuildMessageReceivedEvent event) {
+        String url = CafeBot.getInteractionHandler().getKissImage();
+        String kisser = user.getName();
+        String kissee = CafeBot.getGeneralHelper().getUser(args.get(0)).getName();
+        String message = "**" + kissee + "**, you have been kissed by **" + kisser + "**!";
+
+        if (args.size() == 1) {
+            event.getChannel().sendMessage(message).embed(kissEmbed(url)).queue();
+        } else {
+            event.getChannel().sendMessage(message).embed(kissWithDescriptionEmbed(url, args)).queue();
+        }
+    }
+
+    @NotNull
+    private MessageEmbed kissEmbed(@NotNull String link) {
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setImage(link);
+        embedBuilder.setColor(CafeBot.getGeneralHelper().getRandomColor());
+        return embedBuilder.build();
+    }
+
+    @NotNull
+    private MessageEmbed kissWithDescriptionEmbed(@NotNull String link, @NotNull ArrayList<String> args) {
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setImage(link);
+        embedBuilder.setColor(CafeBot.getGeneralHelper().getRandomColor());
+
+        StringBuilder descriptionBuilder = new StringBuilder();
+        descriptionBuilder.append("\"");
+
+        for (int i = 1; i < args.size(); i++) {
+            descriptionBuilder.append(args.get(i));
+            if (i != args.size() - 1) {
+                descriptionBuilder.append(" ");
+            }
+        }
+
+        descriptionBuilder.append("\"");
+        embedBuilder.setDescription(descriptionBuilder.toString());
+        return embedBuilder.build();
+    }
+
+    @Override
+    public String getName() {
+        return "hug";
+    }
+
+    @Override
+    public ArrayList<String> getAliases() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public String getDescription() {
+        return "Hug someone!";
+    }
+
+    @Override
+    public String exampleUsage() {
+        return "`!!hug @beanbeanjuice` or `!!hug @beanbeanjuice You're so cool`";
+    }
+
+    @Override
+    public Usage getUsage() {
+        Usage usage = new Usage();
+        usage.addUsage(CommandType.USER, "User Mention", true);
+        usage.addUsage(CommandType.SENTENCE, "Extra Message", false);
+        return usage;
+    }
+
+    @Override
+    public CategoryType getCategoryType() {
+        return CategoryType.INTERACTION;
+    }
+
+}
