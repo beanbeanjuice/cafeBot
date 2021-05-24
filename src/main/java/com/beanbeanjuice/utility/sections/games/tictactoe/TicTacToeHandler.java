5e71091ed5cb9c0d967b6813b9f931279afff18c
beanbeanjuice <beanbeanjuice@outlook.com>
2021-05-24 07:01:59 -0400

    Updated javadocs and Cleaned Up Code

--- src/main/java/com/beanbeanjuice/utility/sections/games/tictactoe/TicTacToeHandler.java
+++ src/main/java/com/beanbeanjuice/utility/sections/games/tictactoe/TicTacToeHandler.java
@@ -4,14 +4,29 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 
+/**
+ * A class used for handling {@link TicTacToeGame}.
+ *
+ * @author beanbeanjuice
+ */
 public class TicTacToeHandler {
 
     private HashMap<String, TicTacToeGame> games;
 
+    /**
+     * Creates a new {@link TicTacToeHandler}.
+     */
     public TicTacToeHandler() {
         games = new HashMap<>();
     }
 
+    /**
+     * Creates a {@link TicTacToeGame}.
+     * @param guildID the ID of the {@link net.dv8tion.jda.api.entities.Guild Guild} to create the {@link TicTacToeGame} in.
+     * @param game The {@link TicTacToeGame}.
+     * @return Whether or not it was successfully created.
+     */
+    @NotNull
     public Boolean createGame(@NotNull String guildID, @NotNull TicTacToeGame game) {
         if (games.get(guildID) == null) {
             games.put(guildID, game);
@@ -21,12 +36,12 @@ public class TicTacToeHandler {
         return false;
     }
 
+    /**
+     * Stops a running {@link TicTacToeGame}.
+     * @param game The {@link TicTacToeGame} to be removed and stopped.
+     */
     public void stopGame(@NotNull TicTacToeGame game) {
         games.remove(game.getGuildID());
-    }
-
-    public Boolean hasGame(@NotNull String guildID) {
-        return games.containsKey(guildID);
     }
 
 }
