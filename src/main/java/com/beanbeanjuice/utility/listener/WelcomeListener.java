162c0b44d121af9e8108122c7d291c04097512a3
beanbeanjuice <beanbeanjuice@outlook.com>
2021-05-27 19:52:15 -0400

    Updated the guild_information.sql File

---
+++ src/main/java/com/beanbeanjuice/utility/listener/WelcomeListener.java
@@ -0,0 +1,17 @@
+package com.beanbeanjuice.utility.listener;
+
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A listener for when someone joins the server.
+ */
+public class WelcomeListener extends ListenerAdapter {
+
+    @Override
+    public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event) {
+        
+    }
+
+}
