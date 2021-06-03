57ee11feb0140f0e0339a05e27dde29765bb7658
beanbeanjuice <beanbeanjuice@outlook.com>
2021-06-03 13:37:32 -0400

    Created the Lick Command Class

---
+++ src/main/java/com/beanbeanjuice/command/interaction/LickCommand.java
@@ -0,0 +1,105 @@
+package com.beanbeanjuice.command.interaction;
+
+import com.beanbeanjuice.CafeBot;
+import com.beanbeanjuice.utility.command.CommandContext;
+import com.beanbeanjuice.utility.command.ICommand;
+import com.beanbeanjuice.utility.command.usage.Usage;
+import com.beanbeanjuice.utility.command.usage.categories.CategoryType;
+import com.beanbeanjuice.utility.command.usage.types.CommandType;
+import com.beanbeanjuice.utility.sections.interaction.InteractionType;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+import java.util.ArrayList;
+
+/**
+ * An {@link ICommand} used to lick people.
+ *
+ * @author beanbeanjuice
+ */
+public class LickCommand implements ICommand {
+
+    @Override
+    public void handle(CommandContext ctx, ArrayList<String> args, User user, GuildMessageReceivedEvent event) {
+        String url = CafeBot.getInteractionHandler().getLickImage();
+        String sender = user.getName();
+
+        ArrayList<User> receivers = new ArrayList<>();
+        int count = 0;
+
+        if (!args.isEmpty()) {
+            while (CafeBot.getGeneralHelper().getUser(args.get(count)) != null) {
+                receivers.add(CafeBot.getGeneralHelper().getUser(args.get(count++)));
+                if (count == args.size()) {
+                    break;
+                }
+            }
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = count; i < args.size(); i++) {
+            stringBuilder.append(args.get(i));
+            if (i != args.size() - 1) {
+                stringBuilder.append(" ");
+            }
+        }
+
+        String message;
+        String footer = null;
+
+        if (receivers.size() == 0) {
+            message = "**" + sender + "** *licked* themselves! Uhhh....";
+        } else {
+            message = "**" + sender + "** *licked* **" + CafeBot.getInteractionHandler().getReceiverString(receivers) + "**!";
+
+            if (receivers.size() == 1) {
+                int sendAmount = CafeBot.getInteractionHandler().getSender(user.getId(), InteractionType.LICK) + 1;
+                int receiveAmount = CafeBot.getInteractionHandler().getReceiver(receivers.get(0).getId(), InteractionType.LICK) + 1;
+
+                CafeBot.getInteractionHandler().updateSender(user.getId(), InteractionType.LICK, sendAmount);
+                CafeBot.getInteractionHandler().updateReceiver(receivers.get(0).getId(), InteractionType.LICK, receiveAmount);
+
+                footer = user.getName() + " licked others " + sendAmount + " times. " + receivers.get(0).getName() + " was licked " + receiveAmount + " times.";
+            }
+        }
+
+        if (stringBuilder.isEmpty()) {
+            event.getChannel().sendMessage(message).embed(CafeBot.getInteractionHandler().actionEmbed(url, footer)).queue();
+        } else {
+            event.getChannel().sendMessage(message).embed(CafeBot.getInteractionHandler().actionWithDescriptionEmbed(url, stringBuilder.toString(), footer)).queue();
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "lick";
+    }
+
+    @Override
+    public ArrayList<String> getAliases() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public String getDescription() {
+        return "Lick someone!";
+    }
+
+    @Override
+    public String exampleUsage() {
+        return "`!!lick @beanbeanjuice` or `!!lick @beanbeanjuice :O`";
+    }
+
+    @Override
+    public Usage getUsage() {
+        Usage usage = new Usage();
+        usage.addUsage(CommandType.SENTENCE, "Users + Extra Message", false);
+        return usage;
+    }
+
+    @Override
+    public CategoryType getCategoryType() {
+        return CategoryType.INTERACTION;
+    }
+
+}
