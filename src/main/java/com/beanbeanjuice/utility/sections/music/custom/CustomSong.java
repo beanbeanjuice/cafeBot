01068232d37b6a4929eb9b1ebec28991fa3e5c1c
beanbeanjuice <beanbeanjuice@outlook.com>
2021-06-04 13:10:39 -0400

    Updated javadocs

--- src/main/java/com/beanbeanjuice/utility/sections/music/custom/CustomSong.java
+++ src/main/java/com/beanbeanjuice/utility/sections/music/custom/CustomSong.java
@@ -1,28 +1,28 @@
 package com.beanbeanjuice.utility.sections.music.custom;
 
 import com.beanbeanjuice.CafeBot;
-import com.beanbeanjuice.utility.logger.LogLevel;
-import com.beanbeanjuice.utility.sections.music.lavaplayer.GuildMusicManager;
-import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
-import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
-import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
-import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
-import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
-import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.User;
 import org.jetbrains.annotations.NotNull;
 
-import java.awt.*;
-import java.util.List;
-
+/**
+ * A {@link CustomSong} class used for the {@link CustomSongManager}.
+ *
+ * @author beanbeanjuice
+ */
 public class CustomSong {
 
-    private String name;
-    private String author;
-    private Long length;
-    private User requester;
+    private final String name;
+    private final String author;
+    private final Long length;
+    private final User requester;
 
+    /**
+     * Creates a new {@link CustomSong} object.
+     * @param name The name of the {@link CustomSong}.
+     * @param author The author for the {@link CustomSong}.
+     * @param length The length in milliseconds of the {@link CustomSong}.
+     * @param requester The {@link User} who requested the {@link CustomSong}.
+     */
     public CustomSong(@NotNull String name, @NotNull String author, @NotNull Long length, @NotNull User requester) {
         this.name = name;
         this.author = author;
@@ -30,26 +30,41 @@ public class CustomSong {
         this.requester = requester;
     }
 
+    /**
+     * @return The name of the {@link CustomSong}.
+     */
     @NotNull
     public String getName() {
         return name;
     }
 
+    /**
+     * @return The author for the {@link CustomSong}.
+     */
     @NotNull
     public String getAuthor() {
         return author;
     }
 
+    /**
+     * @return The formatted time length for the {@link CustomSong}.
+     */
     @NotNull
     public String getLengthString() {
         return CafeBot.getGeneralHelper().formatTime(length);
     }
 
+    /**
+     * @return The search term for the {@link CustomSong}.
+     */
     @NotNull
     public String getSearchString() {
         return "ytsearch:" + name + " by " + author;
     }
 
+    /**
+     * @return The person who requested the {@link CustomSong}.
+     */
     @NotNull
     public User getRequester() {
         return requester;
