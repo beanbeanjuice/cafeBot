71287dbfd03455c1e8264f316db0b756f8b46eea
beanbeanjuice <beanbeanjuice@outlook.com>
2021-06-17 22:20:12 -0400

    Made the CafeBot Class Separate from Main

--- src/test/java/SQLServerTest.java
+++ src/test/java/SQLServerTest.java
@@ -1,28 +1,20 @@
 import com.beanbeanjuice.CafeBot;
-import com.beanbeanjuice.utility.sql.SQLServer;
+import com.beanbeanjuice.utility.logger.LogLevel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class SQLServerTest {
+import javax.security.auth.login.LoginException;
 
-    private static final String SQL_URL = System.getenv("CAFEBOT_MYSQL_URL");
-    private static final String SQL_PORT = System.getenv("CAFEBOT_MYSQL_PORT");
-    private static final String SQL_USERNAME = System.getenv("CAFEBOT_MYSQL_USERNAME");
-    private static final String SQL_PASSWORD = System.getenv("CAFEBOT_MYSQL_PASSWORD");
-    private static final boolean SQL_ENCRYPT = Boolean.parseBoolean(System.getenv("CAFEBOT_MYSQL_ENCRYPT"));
+public class SQLServerTest {
 
     @Test
     @DisplayName("SQL Server Test")
-    public void test() {
-        SQLServer sqlServer = new SQLServer(SQL_URL, SQL_PORT, SQL_ENCRYPT, SQL_USERNAME, SQL_PASSWORD);
-        Assertions.assertNotNull(SQL_URL, "URL is null.");
-        Assertions.assertNotNull(SQL_PORT, "PORT is null.");
-        Assertions.assertTrue(SQL_ENCRYPT, "SQL encryption is disabled.");
-        Assertions.assertNotNull(SQL_USERNAME, "USERNAME is null.");
-        Assertions.assertNotNull(SQL_PASSWORD, "PASSWORD is null.");
-        Assertions.assertTrue(sqlServer.startConnection());
-        Assertions.assertTrue(sqlServer.testConnection());
+    public void test() throws LoginException, InterruptedException {
+        new CafeBot();
+        CafeBot.getLogManager().log(this.getClass(), LogLevel.INFO, "Testing the Beta SQL Database Connection...");
+        Assertions.assertTrue(CafeBot.getSQLServer().testConnection());
+        CafeBot.getLogManager().log(this.getClass(), LogLevel.OKAY, "Database is online!");
     }
 
 }
