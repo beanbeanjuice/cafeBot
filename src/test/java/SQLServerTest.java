9ab26459f7397d382a8b3de15c7afafa1bf6b7a5
beanbeanjuice <beanbeanjuice@outlook.com>
2021-06-29 06:53:55 -0400

    Updated GitHub Workflows

--- src/test/java/SQLServerTest.java
+++ src/test/java/SQLServerTest.java
@@ -11,10 +11,10 @@ public class SQLServerTest {
     @Test
     @DisplayName("SQL Server Test")
     public void test() throws LoginException, InterruptedException {
-//        new CafeBot();
-//        CafeBot.getLogManager().log(this.getClass(), LogLevel.INFO, "Testing the Beta SQL Database Connection...");
-//        Assertions.assertTrue(CafeBot.getSQLServer().testConnection());
-//        CafeBot.getLogManager().log(this.getClass(), LogLevel.OKAY, "Database is online!");
+        new CafeBot();
+        CafeBot.getLogManager().log(this.getClass(), LogLevel.INFO, "Testing the Beta SQL Database Connection...");
+        Assertions.assertTrue(CafeBot.getSQLServer().testConnection());
+        CafeBot.getLogManager().log(this.getClass(), LogLevel.OKAY, "Database is online!");
     }
 
 }
