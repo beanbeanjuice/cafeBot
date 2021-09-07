0b3733bb68851e4c8a2b9bca6a8ec2307f942e36
beanbeanjuice <beanbeanjuice@outlook.com>
2021-09-06 19:24:10 -0700

    Generated User Code

--- src/test/java/TipTest.java
+++ src/test/java/TipTest.java
@@ -1,3 +1,4 @@
+import com.beanbeanjuice.cafeapi.cafebot.words.Word;
 import com.beanbeanjuice.utility.sections.cafe.ServeHandler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -8,12 +9,10 @@ public class TipTest {
     @Test
     @DisplayName("Tip Test")
     public void tipTest() {
-
         ServeHandler serveHandler = new ServeHandler();
 
-
         boolean answer = false;
-        Double tip = serveHandler.calculateTip(word1);
+        Double tip = serveHandler.calculateTip(new Word("bruh", 1000));
 
         if (tip > 5 && tip < 30) {
             answer = true;
