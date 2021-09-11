09a00a0c39f7b85e16537b454e5036bf2b171e40
William Orlopp <beanbeanjuice@outlook.com>
2021-09-11 05:31:08 -0700

    Revamped the Interaction System
    The interaction system now uses the same basic class, instead of
    different classes for each one. This removes the complicated nature of
    creating interactions.

--- src/main/java/com/beanbeanjuice/command/interaction/HugCommand.java
+++ src/main/java/com/beanbeanjuice/command/interaction/HugCommand.java
@@ -1,12 +1,13 @@
 package com.beanbeanjuice.command.interaction;
 
 import com.beanbeanjuice.CafeBot;
+import com.beanbeanjuice.cafeapi.cafebot.interactions.InteractionType;
 import com.beanbeanjuice.utility.command.CommandContext;
 import com.beanbeanjuice.utility.command.ICommand;
 import com.beanbeanjuice.utility.command.usage.Usage;
 import com.beanbeanjuice.utility.command.usage.categories.CategoryType;
 import com.beanbeanjuice.utility.command.usage.types.CommandType;
-import com.beanbeanjuice.utility.sections.interaction.InteractionType;
+import com.beanbeanjuice.utility.sections.interaction.Interaction;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
@@ -21,53 +22,13 @@ public class HugCommand implements ICommand {
 
     @Override
     public void handle(CommandContext ctx, ArrayList<String> args, User user, GuildMessageReceivedEvent event) {
-        String url = CafeBot.getInteractionHandler().getHugImage();
-        String sender = user.getName();
-
-        ArrayList<User> receivers = new ArrayList<>();
-        int count = 0;
-
-        if (!args.isEmpty()) {
-            while (CafeBot.getGeneralHelper().getUser(args.get(count)) != null) {
-                receivers.add(CafeBot.getGeneralHelper().getUser(args.get(count++)));
-                if (count == args.size()) {
-                    break;
-                }
-            }
-        }
-
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = count; i < args.size(); i++) {
-            stringBuilder.append(args.get(i));
-            if (i != args.size() - 1) {
-                stringBuilder.append(" ");
-            }
-        }
-
-        String message;
-        String footer = null;
-
-        if (receivers.size() == 0) {
-            message = "**" + sender + "** *hugged* themselves! How... sad...";
-        } else {
-            message = "**" + sender + "** *hugged* **" + CafeBot.getInteractionHandler().getReceiverString(receivers) + "**!";
-
-            if (receivers.size() == 1) {
-                int sendAmount = CafeBot.getInteractionHandler().getSender(user.getId(), InteractionType.HUG) + 1;
-                int receiveAmount = CafeBot.getInteractionHandler().getReceiver(receivers.get(0).getId(), InteractionType.HUG) + 1;
-
-                CafeBot.getInteractionHandler().updateSender(user.getId(), InteractionType.HUG, sendAmount);
-                CafeBot.getInteractionHandler().updateReceiver(receivers.get(0).getId(), InteractionType.HUG, receiveAmount);
-
-                footer = user.getName() + " hugged others " + sendAmount + " times. " + receivers.get(0).getName() + " was hugged " + receiveAmount + " times.";
-            }
-        }
-
-        if (stringBuilder.isEmpty()) {
-            event.getChannel().sendMessage(message).embed(CafeBot.getInteractionHandler().actionEmbed(url, footer)).queue();
-        } else {
-            event.getChannel().sendMessage(message).embed(CafeBot.getInteractionHandler().actionWithDescriptionEmbed(url, stringBuilder.toString(), footer)).queue();
-        }
+        new Interaction(InteractionType.HUG,
+                "**{sender}** *hugged* themselves! Umm... how?",
+                "**{sender}** *hugged* **{receiver}**!",
+                "{sender} hugged others {amount_sent} times. {receiver} was hugged {amount_received} times.",
+                user,
+                args,
+                event.getChannel());
     }
 
     @Override
--- src/main/java/com/beanbeanjuice/command/interaction/WelcomeCommand.java
+++ src/main/java/com/beanbeanjuice/command/interaction/WelcomeCommand.java
@@ -1,12 +1,13 @@
 package com.beanbeanjuice.command.interaction;
 
 import com.beanbeanjuice.CafeBot;
+import com.beanbeanjuice.cafeapi.cafebot.interactions.InteractionType;
 import com.beanbeanjuice.utility.command.CommandContext;
 import com.beanbeanjuice.utility.command.ICommand;
 import com.beanbeanjuice.utility.command.usage.Usage;
 import com.beanbeanjuice.utility.command.usage.categories.CategoryType;
 import com.beanbeanjuice.utility.command.usage.types.CommandType;
-import com.beanbeanjuice.utility.sections.interaction.InteractionType;
+import com.beanbeanjuice.utility.sections.interaction.Interaction;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
@@ -21,53 +22,13 @@ public class WelcomeCommand implements ICommand {
 
     @Override
     public void handle(CommandContext ctx, ArrayList<String> args, User user, GuildMessageReceivedEvent event) {
-        String url = CafeBot.getInteractionHandler().getWelcomeImage();
-        String sender = user.getName();
-
-        ArrayList<User> receivers = new ArrayList<>();
-        int count = 0;
-
-        if (!args.isEmpty()) {
-            while (CafeBot.getGeneralHelper().getUser(args.get(count)) != null) {
-                receivers.add(CafeBot.getGeneralHelper().getUser(args.get(count++)));
-                if (count == args.size()) {
-                    break;
-                }
-            }
-        }
-
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = count; i < args.size(); i++) {
-            stringBuilder.append(args.get(i));
-            if (i != args.size() - 1) {
-                stringBuilder.append(" ");
-            }
-        }
-
-        String message;
-        String footer = null;
-
-        if (receivers.size() == 0) {
-            message = "**" + sender + "** *welcomed* themselves! That's sad.";
-        } else {
-            message = "**" + sender + "** *welcomed* **" + CafeBot.getInteractionHandler().getReceiverString(receivers) + "**!";
-
-            if (receivers.size() == 1) {
-                int sendAmount = CafeBot.getInteractionHandler().getSender(user.getId(), InteractionType.WELCOME) + 1;
-                int receiveAmount = CafeBot.getInteractionHandler().getReceiver(receivers.get(0).getId(), InteractionType.WELCOME) + 1;
-
-                CafeBot.getInteractionHandler().updateSender(user.getId(), InteractionType.WELCOME, sendAmount);
-                CafeBot.getInteractionHandler().updateReceiver(receivers.get(0).getId(), InteractionType.WELCOME, receiveAmount);
-
-                footer = user.getName() + " welcomed others " + sendAmount + " times. " + receivers.get(0).getName() + " was welcomed " + receiveAmount + " times.";
-            }
-        }
-
-        if (stringBuilder.isEmpty()) {
-            event.getChannel().sendMessage(message).embed(CafeBot.getInteractionHandler().actionEmbed(url, footer)).queue();
-        } else {
-            event.getChannel().sendMessage(message).embed(CafeBot.getInteractionHandler().actionWithDescriptionEmbed(url, stringBuilder.toString(), footer)).queue();
-        }
+        new Interaction(InteractionType.WELCOME,
+                "**{sender}** *welcomed* themselves! Awww.. won't someone besides yourself welcome you, **{sender}**?",
+                "**{sender}** *welcomed* **{receiver}**!",
+                "{sender} welcomed others {amount_sent} times. {receiver} was welcomed {amount_received} times.",
+                user,
+                args,
+                event.getChannel());
     }
 
     @Override
