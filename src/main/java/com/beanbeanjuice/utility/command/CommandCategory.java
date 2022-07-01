90aaaf7436924671a4465fecb90d9d0b96064b63
beanbeanjuice <beanbeanjuice@outlook.com>
2022-07-01 00:21:47 -0400

    Added Listeners and GuildHandler

--- src/main/java/com/beanbeanjuice/utility/command/CommandCategory.java
+++ src/main/java/com/beanbeanjuice/utility/command/CommandCategory.java
@@ -10,10 +10,8 @@ public enum CommandCategory {
     GAMES("Commands used for small games.", "https://cdn.beanbeanjuice.com/images/cafeBot/category_type/games.png"),
     SOCIAL("Commands used for social media stuff", "https://cdn.beanbeanjuice.com/images/cafeBot/category_type/social.gif"),
     INTERACTION("Commands used for user interactions.", "https://cdn.beanbeanjuice.com/images/cafeBot/category_type/interaction.png"),
-    MUSIC("THIS IS EMPTY BECAUSE YOUTUBE IS TAKING DOWN MUSIC BOTS", "https://cdn.beanbeanjuice.com/images/cafeBot/category_type/music.webp"),
     TWITCH("Commands used for twitch.", "https://cdn.beanbeanjuice.com/images/cafeBot/category_type/twitch.jpg"),
-    MODERATION("Commands used for moderation.", "https://cdn.beanbeanjuice.com/images/cafeBot/category_type/moderation.png"),
-    EXPERIMENTAL("Commands that are still experimental, but *may* work.", "https://cdn.beanbeanjuice.com/images/cafeBot/category_type/experimental.gif");
+    MODERATION("Commands used for moderation.", "https://cdn.beanbeanjuice.com/images/cafeBot/category_type/moderation.png");
 
     private final String message;
     private final String link;
---
+++ src/main/java/com/beanbeanjuice/utility/handler/guild/CustomChannel.java
@@ -0,0 +1,38 @@
+package com.beanbeanjuice.utility.handler.guild;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * An enum used for {@link CustomChannel}.
+ */
+public enum CustomChannel {
+
+    LIVE("Live Channel"),
+    UPDATE("Update Channel"),
+    COUNTING("Counting Channel"),
+    POLL("Poll Channel"),
+    RAFFLE("Raffle Channel"),
+    BIRTHDAY("Birthday Channel"),
+    WELCOME("Welcome Channel"),
+    LOG("Log Channel"),
+    VENTING("Venting Channel"),
+    DAILY("Daily Channel");
+
+    private final String name;
+
+    /**
+     * Creates a new {@link CustomChannel} object.
+     * @param name The name of the enum.
+     */
+    CustomChannel(@NotNull String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return The formatted name of the enum.
+     */
+    public String getName() {
+        return name;
+    }
+
+}
