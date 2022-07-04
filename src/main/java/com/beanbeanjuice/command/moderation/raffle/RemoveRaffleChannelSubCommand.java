7e34e5a8e32b0be3b75bb17ff5b57d49785cce8b
beanbeanjuice <beanbeanjuice@outlook.com>
2022-07-04 05:31:29 -0400

    Created the RaffleChannel Command

---
+++ src/main/java/com/beanbeanjuice/command/moderation/raffle/RemoveRaffleChannelSubCommand.java
@@ -0,0 +1,61 @@
+package com.beanbeanjuice.command.moderation.raffle;
+
+import com.beanbeanjuice.Bot;
+import com.beanbeanjuice.utility.command.CommandCategory;
+import com.beanbeanjuice.utility.command.ISubCommand;
+import com.beanbeanjuice.utility.helper.Helper;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * An {@link ISubCommand} used to remove the {@link com.beanbeanjuice.utility.section.moderation.raffle.Raffle Raffle} {@link net.dv8tion.jda.api.entities.TextChannel TextChannel}.
+ *
+ * @author beanbeanjuice
+ */
+public class RemoveRaffleChannelSubCommand implements ISubCommand {
+
+    @Override
+    public void handle(@NotNull SlashCommandInteractionEvent event) {
+
+        // Attempt to remove the raffle channel from the database.
+        if (Bot.getGuildHandler().getCustomGuild(event.getGuild()).setRaffleChannel("0")) {
+            event.getHook().sendMessageEmbeds(Helper.successEmbed(
+                    "Removed Raffle Channel",
+                    "The raffle channel has been successfully removed."
+            )).queue();
+            return;
+        }
+        event.getHook().sendMessageEmbeds(Helper.sqlServerError()).queue();
+    }
+
+    @NotNull
+    @Override
+    public String getDescription() {
+        return "Remove the raffle channel!";
+    }
+
+    @NotNull
+    @Override
+    public String exampleUsage() {
+        return "`/raffle-channel remove`";
+    }
+
+    @NotNull
+    @Override
+    public CommandCategory getCategoryType() {
+        return CommandCategory.MODERATION;
+    }
+
+    @NotNull
+    @Override
+    public Boolean allowDM() {
+        return false;
+    }
+
+    @NotNull
+    @Override
+    public String getName() {
+        return "remove";
+    }
+
+}
