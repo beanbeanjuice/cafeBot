c9a769a08aa91f8cd9d329efef094abe35cf29e9
beanbeanjuice <beanbeanjuice@outlook.com>
2022-07-04 05:31:29 -0400

    Created the RaffleChannel Command

---
+++ src/main/java/com/beanbeanjuice/command/moderation/raffle/RaffleChannelCommand.java
@@ -0,0 +1,70 @@
+package com.beanbeanjuice.command.moderation.raffle;
+
+import com.beanbeanjuice.utility.command.CommandCategory;
+import com.beanbeanjuice.utility.command.ICommand;
+import com.beanbeanjuice.utility.command.ISubCommand;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+
+/**
+ * An {@link ICommand} used to set/remove the {@link com.beanbeanjuice.utility.section.moderation.raffle.Raffle Raffle} {@link net.dv8tion.jda.api.entities.TextChannel TextChannel}.
+ *
+ * @author beanbeanjuice
+ */
+public class RaffleChannelCommand implements ICommand {
+
+    @Override
+    public void handle(@NotNull SlashCommandInteractionEvent event) { }
+
+    @NotNull
+    @Override
+    public String getDescription() {
+        return "Set or remove the raffle channel!";
+    }
+
+    @NotNull
+    @Override
+    public String exampleUsage() {
+        return "`/raffle-channel set` or `/raffle-channel remove`";
+    }
+
+    @NotNull
+    @Override
+    public CommandCategory getCategoryType() {
+        return CommandCategory.MODERATION;
+    }
+
+    @NotNull
+    @Override
+    public ArrayList<ISubCommand> getSubCommands() {
+        ArrayList<ISubCommand> subCommands = new ArrayList<>();
+        subCommands.add(new SetRaffleChannelSubCommand());
+        subCommands.add(new RemoveRaffleChannelSubCommand());
+        return subCommands;
+    }
+
+    @NotNull
+    @Override
+    public Boolean allowDM() {
+        return false;
+    }
+
+    @NotNull
+    @Override
+    public Boolean isHidden() {
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public ArrayList<Permission> getPermissions() {
+        ArrayList<Permission> permissions = new ArrayList<>();
+        permissions.add(Permission.MANAGE_SERVER);
+        return permissions;
+    }
+
+}
