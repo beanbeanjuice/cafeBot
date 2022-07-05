814d0943018e966a912db4b1ba79553981d8cb81
beanbeanjuice <beanbeanjuice@outlook.com>
2022-07-05 03:05:54 -0400

    Created the BotVersion Command

---
+++ src/main/java/com/beanbeanjuice/command/generic/BotVersionCommand.java
@@ -0,0 +1,79 @@
+package com.beanbeanjuice.command.generic;
+
+import com.beanbeanjuice.Bot;
+import com.beanbeanjuice.utility.api.GitHubReleaseHelper;
+import com.beanbeanjuice.utility.command.CommandCategory;
+import com.beanbeanjuice.utility.command.ICommand;
+import com.beanbeanjuice.utility.helper.Helper;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.concurrent.CompletionException;
+
+/**
+ * An {@link ICommand} used to get a specific release version from the bot.
+ *
+ * @author beanbeanjuice
+ */
+public class BotVersionCommand implements ICommand {
+
+    @Override
+    public void handle(@NotNull SlashCommandInteractionEvent event) {
+
+        String version = Bot.BOT_VERSION;
+        try {
+            version = event.getOption("bot_version").getAsString();
+        } catch (NullPointerException ignored) {}
+
+        try {
+            event.getChannel().sendMessageEmbeds(new GitHubReleaseHelper().getVersion(version)).queue();
+        } catch (CompletionException e) {
+            event.getChannel().sendMessageEmbeds(Helper.errorEmbed(
+                    "Update Not Found",
+                    "There is no update corresponding to that version number..."
+            )).queue();
+        }
+    }
+
+    @NotNull
+    @Override
+    public String getDescription() {
+        return "Show release information for the bot!";
+    }
+
+    @NotNull
+    @Override
+    public String exampleUsage() {
+        return "`/bot-version` or `/bot-version v1.1.1`";
+    }
+
+    @NotNull
+    @Override
+    public ArrayList<OptionData> getOptions() {
+        ArrayList<OptionData> options = new ArrayList<>();
+        options.add(new OptionData(OptionType.STRING, "version_number", "Specific version number for the bot.", false));
+        return options;
+    }
+
+    @NotNull
+    @Override
+    public CommandCategory getCategoryType() {
+        return CommandCategory.GENERIC;
+    }
+
+    @NotNull
+    @Override
+    public Boolean allowDM() {
+        return true;
+    }
+
+    @NotNull
+    @Override
+    public Boolean isHidden() {
+        return true;
+    }
+
+}
