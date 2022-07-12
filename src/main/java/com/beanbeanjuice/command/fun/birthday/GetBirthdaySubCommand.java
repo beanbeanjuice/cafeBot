879a91d58816e1b1c1e5f40551e6f9bfe8d920a9
beanbeanjuice <beanbeanjuice@outlook.com>
2022-07-12 12:56:16 -0400

    Updated Contributing.MD

--- src/main/java/com/beanbeanjuice/command/fun/birthday/GetBirthdaySubCommand.java
+++ src/main/java/com/beanbeanjuice/command/fun/birthday/GetBirthdaySubCommand.java
@@ -22,6 +22,10 @@ import java.util.ArrayList;
  */
 public class GetBirthdaySubCommand implements ISubCommand {
 
+    /**
+     * 
+     * @param event The {@link SlashCommandInteractionEvent}.
+     */
     @Override
     public void handle(@NotNull SlashCommandInteractionEvent event) {
         User user = event.getUser();
