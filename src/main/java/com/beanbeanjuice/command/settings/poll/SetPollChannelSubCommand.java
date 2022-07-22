a46ed298681bc4e83dbeb3f3ac2f5e7118397350
beanbeanjuice <beanbeanjuice@outlook.com>
2022-07-22 13:58:07 -0400

    Fixed the Daily Channel Issue
    The daily channel can no longer be set to channels that are already
    custom channels.

--- src/main/java/com/beanbeanjuice/command/settings/poll/SetPollChannelSubCommand.java
+++ src/main/java/com/beanbeanjuice/command/settings/poll/SetPollChannelSubCommand.java
@@ -5,8 +5,14 @@ import com.beanbeanjuice.utility.command.ISubCommand;
 import com.beanbeanjuice.utility.handler.guild.GuildHandler;
 import com.beanbeanjuice.utility.helper.Helper;
 import com.beanbeanjuice.utility.section.moderation.poll.Poll;
+import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
 
 /**
  * An {@link ISubCommand} used to set the {@link Poll Poll} {@link net.dv8tion.jda.api.entities.TextChannel TextChannel}.
@@ -17,9 +23,18 @@ public class SetPollChannelSubCommand implements ISubCommand {
 
     @Override
     public void handle(@NotNull SlashCommandInteractionEvent event) {
+        TextChannel channel = event.getTextChannel();
+        if (event.getOption("poll_channel") != null)
+            channel = event.getOption("poll_channel").getAsTextChannel();
+
+        // If the channel is already set, notify them that this cannot be done.
+        if (GuildHandler.getCustomGuild(event.getGuild()).isDailyChannel(channel.getId())) {
+            event.getHook().sendMessageEmbeds(Helper.alreadyDailyChannel()).queue();
+            return;
+        }
 
         // Makes sure the poll channel has been added to the database.
-        if (GuildHandler.getCustomGuild(event.getGuild()).setPollChannel(event.getChannel().getId())) {
+        if (GuildHandler.getCustomGuild(event.getGuild()).setPollChannel(channel.getId())) {
             event.getHook().sendMessageEmbeds(Helper.successEmbed(
                     "Set Poll Channel",
                     "The current channel has been set to a `poll` channel!"
@@ -28,6 +43,15 @@ public class SetPollChannelSubCommand implements ISubCommand {
         }
 
         event.getHook().sendMessageEmbeds(Helper.sqlServerError()).queue();
+    }
+
+    @NotNull
+    @Override
+    public ArrayList<OptionData> getOptions() {
+        ArrayList<OptionData> options = new ArrayList<>();
+        options.add(new OptionData(OptionType.CHANNEL, "poll_channel", "The text channel to send poll information to.", false)
+                .setChannelTypes(ChannelType.TEXT));
+        return options;
     }
 
     @NotNull
--- src/main/java/com/beanbeanjuice/command/settings/welcome/SetWelcomeChannelSubCommand.java
+++ src/main/java/com/beanbeanjuice/command/settings/welcome/SetWelcomeChannelSubCommand.java
@@ -4,8 +4,14 @@ import com.beanbeanjuice.utility.command.CommandCategory;
 import com.beanbeanjuice.utility.command.ISubCommand;
 import com.beanbeanjuice.utility.handler.guild.GuildHandler;
 import com.beanbeanjuice.utility.helper.Helper;
+import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
 
 /**
  * An {@link ISubCommand} used to set the welcome {@link net.dv8tion.jda.api.entities.TextChannel TextChannel}
@@ -17,7 +23,18 @@ public class SetWelcomeChannelSubCommand implements ISubCommand {
 
     @Override
     public void handle(@NotNull SlashCommandInteractionEvent event) {
-        if (GuildHandler.getCustomGuild(event.getGuild()).setWelcomeChannelID(event.getChannel().getId())) {
+
+        TextChannel channel = event.getTextChannel();
+        if (event.getOption("welcome_channel") != null)
+            channel = event.getOption("welcome_channel").getAsTextChannel();
+
+        // If the channel is already set, notify them that this cannot be done.
+        if (GuildHandler.getCustomGuild(event.getGuild()).isDailyChannel(channel.getId())) {
+            event.getHook().sendMessageEmbeds(Helper.alreadyDailyChannel()).queue();
+            return;
+        }
+
+        if (GuildHandler.getCustomGuild(event.getGuild()).setWelcomeChannelID(channel.getId())) {
             event.getHook().sendMessageEmbeds(Helper.successEmbed(
                     "Set Welcome Channel",
                     "This channel has been set to the welcome channel! Make sure to " +
@@ -30,8 +47,17 @@ public class SetWelcomeChannelSubCommand implements ISubCommand {
 
     @NotNull
     @Override
+    public ArrayList<OptionData> getOptions() {
+        ArrayList<OptionData> options = new ArrayList<>();
+        options.add(new OptionData(OptionType.CHANNEL, "welcome_channel", "The text channel to send welcome information to.", false)
+                .setChannelTypes(ChannelType.TEXT));
+        return options;
+    }
+
+    @NotNull
+    @Override
     public String getDescription() {
-        return "Set the current channel to welcome channel!";
+        return "Set a channel to welcome channel!";
     }
 
     @NotNull
