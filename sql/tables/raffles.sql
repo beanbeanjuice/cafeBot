c741b54135166c10bab4606da5a611add345ea2a
beanbeanjuice <beanbeanjuice@outlook.com>
2023-07-08 22:27:10 -0700

    Moved SQL Files to API Repository

--- sql/tables/raffles.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE raffles
-(guild_id BIGINT,
-message_id BIGINT,
-ending_time TIMESTAMP,
-winner_amount INT);
--- sql/tables/welcome_information.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE welcome_information
-(guild_id BIGINT,
-description TEXT null,
-thumbnail_url TEXT null,
-image_url TEXT null,
-message TEXT null);
