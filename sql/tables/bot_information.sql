75a90f58010b780fc333e48955b71dbae05d53ac
beanbeanjuice <beanbeanjuice@outlook.com>
2023-07-08 22:27:10 -0700

    Moved SQL Files to API Repository

--- sql/tables/bot_information.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE bot_information
-(id INT default 1,
- version TEXT);
--- sql/tables/minigames_win_streaks.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE minigames_win_streaks
-(user_id BIGINT,
-tic_tac_toe INT DEFAULT 0,
-connect_four INT DEFAULT 0);
