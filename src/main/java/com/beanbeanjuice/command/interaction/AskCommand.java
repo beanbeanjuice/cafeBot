004612fcc4a2964c851faeed0f6e7ed391575d87
beanbeanjuice <beanbeanjuice@outlook.com>
2023-09-18 17:23:59 -0700

    Added New Interaction Commands
    Additionally, JDA was updated to the latest version.

---
+++ src/main/java/com/beanbeanjuice/command/interaction/AskCommand.java
@@ -0,0 +1,59 @@
+package com.beanbeanjuice.command.interaction;
+
+import com.beanbeanjuice.cafeapi.cafebot.interactions.InteractionType;
+import com.beanbeanjuice.utility.command.CommandCategory;
+import com.beanbeanjuice.utility.command.ICommand;
+import com.beanbeanjuice.utility.section.interaction.Interaction;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class AskCommand implements ICommand {
+
+    @Override
+    public void handle(@NotNull SlashCommandInteractionEvent event) {
+        new Interaction(InteractionType.ASK,
+                "**{sender}** is asking a question!",
+                "**{sender}** is asking **{receiver}** a question!",
+                "{sender} asked {amount_sent} questions. {receiver} was questioned {amount_received} times.",
+                "Umm... I don't wanna answer any questions... 😡",
+                event);
+    }
+
+    @NotNull
+    @Override
+    public String getDescription() {
+        return "Ask something!";
+    }
+
+    @NotNull
+    @Override
+    public String exampleUsage() {
+        return "`/ask` or `/ask @beanbeanjuice` or `/ask @beanbeanjuice How is your day?`";
+    }
+
+    @NotNull
+    @Override
+    public ArrayList<OptionData> getOptions() {
+        ArrayList<OptionData> options = new ArrayList<>();
+        options.add(new OptionData(OptionType.USER, "receiver", "The person to ask the question toward.", false, false));
+        options.add(new OptionData(OptionType.STRING, "message", "An optional message to add.", false, false));
+        return options;
+    }
+
+    @NotNull
+    @Override
+    public CommandCategory getCategoryType() {
+        return CommandCategory.INTERACTION;
+    }
+
+    @NotNull
+    @Override
+    public Boolean allowDM() {
+        return true;
+    }
+
+}
---
+++ src/main/java/com/beanbeanjuice/command/interaction/ShootCommand.java
@@ -0,0 +1,59 @@
+package com.beanbeanjuice.command.interaction;
+
+import com.beanbeanjuice.cafeapi.cafebot.interactions.InteractionType;
+import com.beanbeanjuice.utility.command.CommandCategory;
+import com.beanbeanjuice.utility.command.ICommand;
+import com.beanbeanjuice.utility.section.interaction.Interaction;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class ShootCommand implements ICommand {
+
+    @Override
+    public void handle(@NotNull SlashCommandInteractionEvent event) {
+        new Interaction(InteractionType.SHOOT,
+                "**{sender}** *SHOT* themselves (in-game)!! WH- WHY WOULD YOU DO THAT??",
+                "**{sender}** *shot* **{receiver}** (in-game)! SOMEONE STOP THEM!!??",
+                "{sender} shot others (in-game) {amount_sent} times. {receiver} was shot (in-game) {amount_received} times.",
+                "YOU CAN TRY AND TAKE ME OUT BUT ANOTHER ONE WILL TAKE MY PLACE, I WON'T FORGET EITHER... <:stab_u:886216384864997406>",
+                event);
+    }
+
+    @NotNull
+    @Override
+    public String getDescription() {
+        return "Shoot (in-game) someone!";
+    }
+
+    @NotNull
+    @Override
+    public String exampleUsage() {
+        return "`/shoot` or `/shoot @beanbeanjuice` or `/shoot @beanbeanjuice HA!`";
+    }
+
+    @NotNull
+    @Override
+    public ArrayList<OptionData> getOptions() {
+        ArrayList<OptionData> options = new ArrayList<>();
+        options.add(new OptionData(OptionType.USER, "receiver", "The person to shoot (in-game).", false, false));
+        options.add(new OptionData(OptionType.STRING, "message", "An optional message to add.", false, false));
+        return options;
+    }
+
+    @NotNull
+    @Override
+    public CommandCategory getCategoryType() {
+        return CommandCategory.INTERACTION;
+    }
+
+    @NotNull
+    @Override
+    public Boolean allowDM() {
+        return true;
+    }
+
+}
