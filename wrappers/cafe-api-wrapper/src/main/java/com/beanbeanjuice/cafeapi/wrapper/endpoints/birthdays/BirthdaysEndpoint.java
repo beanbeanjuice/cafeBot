78e91a914aaaa01e6aa91077f42d50b0855915b8
beanbeanjuice <beanbeanjuice@outlook.com>
2024-06-23 20:15:28 -0400

    Moved the Birthdays Endpoint to Completable Futures

--- wrappers/cafe-api-wrapper/src/main/java/com/beanbeanjuice/cafeapi/wrapper/endpoints/birthdays/BirthdaysEndpoint.java
+++ wrappers/cafe-api-wrapper/src/main/java/com/beanbeanjuice/cafeapi/wrapper/endpoints/birthdays/BirthdaysEndpoint.java
@@ -10,9 +10,9 @@ import com.beanbeanjuice.cafeapi.wrapper.exception.api.*;
 import com.beanbeanjuice.cafeapi.wrapper.utility.Time;
 import com.fasterxml.jackson.databind.JsonNode;
 
-import java.text.ParseException;
 import java.util.*;
-import java.util.function.Consumer;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 
 /**
  * A class used for {@link Birthday} requests to the {@link CafeAPI CafeAPI}.
@@ -38,67 +38,31 @@ public class BirthdaysEndpoint extends CafeEndpoint {
      * @throws AuthorizationException Thrown when the {@link String apiKey} is invalid.
      * @throws ResponseException Thrown when there is a generic server-side {@link CafeException}.
      */
-    public HashMap<String, Birthday> getAllBirthdays() throws AuthorizationException, ResponseException {
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
+    public CompletableFuture<HashMap<String, Birthday>> getAllBirthdays() throws AuthorizationException, ResponseException {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
                 .setRoute("/birthdays")
                 .setAuthorization(apiKey)
-                .build().orElseThrow();
-
-        return requestToBirthdayMap(request);
-    }
-
-    public void getAllBirthdaysAsync(Consumer<HashMap<String, Birthday>> successFunction, Consumer<Exception> errorFunction) {
-        RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
-                .setRoute("/birthdays")
-                .setAuthorization(apiKey)
-                .onSuccess((request) -> successFunction.accept(requestToBirthdayMap(request)))
-                .onError(errorFunction)
-                .buildAsync();
-    }
-
-    public void getAllBirthdaysAsync(Consumer<HashMap<String, Birthday>> successFunction) {
-        RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
-                .setRoute("/birthdays")
-                .setAuthorization(apiKey)
-                .buildAsync((request) -> successFunction.accept(requestToBirthdayMap(request)));
+                .buildAsync()
+                .thenApplyAsync((optionalRequest) -> {
+                    if (optionalRequest.isPresent()) return requestToBirthdayMap(optionalRequest.get());
+                    throw new CompletionException("Error getting all birthdays. Request is empty.", null);
+                });
     }
 
     /**
      * Retrieves the {@link Birthday} for a specified {@link String userID}.
      * @param userID The specified {@link String userID}.
      * @return The {@link Birthday} for the specified {@link String userID}.
-     * @throws AuthorizationException Thrown when the {@link String apiKey} is invalid.
-     * @throws ResponseException Thrown when there is a generic server-side {@link CafeException}.
-     * @throws NotFoundException Thrown when the {@link Birthday} for the specified {@link String userID} does not exist.
-     * @throws ParseException Thrown when there was an error parsing the {@link Birthday}.
      */
-    public Optional<Birthday> getUserBirthday(final String userID)
-            throws AuthorizationException, ResponseException, NotFoundException {
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
+    public CompletableFuture<Optional<Birthday>> getUserBirthday(final String userID) {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
                 .setRoute("/birthdays/" + userID)
                 .setAuthorization(apiKey)
-                .build().orElseThrow();
-
-        return parseBirthday(request.getData().get("birthday"));
-    }
-
-    public void getUserBirthdayAsync(final String userID, final Consumer<Birthday> successFunction, final Consumer<Exception> errorFunction) {
-        RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
-                .setRoute("/birthdays/" + userID)
-                .setAuthorization(apiKey)
-                .onSuccess((request) -> parseBirthday(request.getData().get("birthday")).ifPresentOrElse(
-                        successFunction,
-                        () -> errorFunction.accept(new ParseException("There was an error parsing the birthday.", 0))
-                ))
-                .onError(errorFunction)
-                .buildAsync();
-    }
-
-    public void getUserBirthdayAsync(final String userID, final Consumer<Birthday> successFunction) {
-        RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
-                .setRoute("/birthdays/" + userID)
-                .setAuthorization(apiKey)
-                .buildAsync((request) -> parseBirthday(request.getData().get("birthday")).ifPresent(successFunction));
+                .buildAsync()
+                .thenApplyAsync((optionalRequest) -> {
+                    if (optionalRequest.isPresent()) return parseBirthday(optionalRequest.get().getData().get("birthday"));
+                    throw new CompletionException("Error getting user birthday. Request is empty.", null);
+                });
     }
 
     /**
@@ -106,60 +70,21 @@ public class BirthdaysEndpoint extends CafeEndpoint {
      * @param userID The specified {@link String userID}.
      * @param birthday The {@link Birthday} for the specified {@link String userID}.
      * @return True, if the {@link Birthday} was successfully updated in the {@link CafeAPI CafeAPI}.
-     * @throws AuthorizationException Thrown when the {@link String apiKey} is invalid.
-     * @throws ResponseException Thrown when there is a generic server-side {@link CafeException}.
-     * @throws NotFoundException Thrown when the {@link Birthday} for the specified {@link String userID} does not exist.
-     * @throws UndefinedVariableException Thrown when a variable is undefined.
-     * @throws TeaPotException Thrown when the {@link BirthdayMonth month} or {@link Integer day} is invalid.
      */
-    public boolean updateUserBirthday(final String userID, final Birthday birthday)
-    throws AuthorizationException, ResponseException, NotFoundException, UndefinedVariableException {
+    public CompletableFuture<Boolean> updateUserBirthday(final String userID, final Birthday birthday) {
         BirthdayMonth month = birthday.getMonth();
         int day = birthday.getDay();
 
-        // TODO: Check for invalid days/month.
-//        // Checking for days in a month.
-//        if (month.getDaysInMonth() < day)
-//            throw new TeaPotException("There are only " + month.getDaysInMonth() + " days in " + month + ", not " + day + ".");
-//
-//        // Checking if the month is valid.
-//        if (month == BirthdayMonth.ERROR)
-//            throw new TeaPotException(month + " is an invalid month.");
-
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.PATCH)
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.PATCH)
                 .setRoute("/birthdays/" + userID)
                 .addParameter("birthday", getBirthdayString(month, day))
                 .addParameter("time_zone", birthday.getTimeZone().getID())
                 .setAuthorization(apiKey)
-                .build().orElseThrow();
-
-        return request.getStatusCode() == 200;
-    }
-
-    public void updateUserBirthdayAsync(final String userID, final Birthday birthday, final Runnable successFunction, final Consumer<Exception> errorFunction) {
-        BirthdayMonth month = birthday.getMonth();
-        int day = birthday.getDay();
-
-        RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.PATCH)
-                .setRoute("/birthdays/" + userID)
-                .addParameter("birthday", getBirthdayString(month, day))
-                .addParameter("time_zone", birthday.getTimeZone().getID())
-                .setAuthorization(apiKey)
-                .onSuccess((request) -> successFunction.run())
-                .onError(errorFunction)
-                .buildAsync();
-    }
-
-    public void updateUserBirthdayAsync(final String userID, final Birthday birthday, final Runnable successFunction) {
-        BirthdayMonth month = birthday.getMonth();
-        int day = birthday.getDay();
-
-        RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.PATCH)
-                .setRoute("/birthdays/" + userID)
-                .addParameter("birthday", getBirthdayString(month, day))
-                .addParameter("time_zone", birthday.getTimeZone().getID())
-                .setAuthorization(apiKey)
-                .buildAsync((request) -> successFunction.run());
+                .buildAsync()
+                .thenApplyAsync((optionalRequest) -> {
+                    if (optionalRequest.isPresent()) return optionalRequest.get().getStatusCode() == 200;
+                    throw new CompletionException("Error updating user birthday. Request is empty.", null);
+                });
     }
 
     /**
@@ -167,38 +92,17 @@ public class BirthdaysEndpoint extends CafeEndpoint {
      * @param userID The specified {@link String userID}.
      * @param alreadyMentioned The new {@link Boolean alreadyMentioned} state.
      * @return True, if the {@link Birthday} was successfully updated in the {@link CafeAPI CafeAPI}.
-     * @throws AuthorizationException Thrown when the {@link String apiKey} is invalid.
-     * @throws ResponseException Thrown when there is a generic server-side {@link CafeException}.
-     * @throws NotFoundException Thrown when the {@link Birthday} does not exist for the specified {@link String userID}.
-     * @throws UndefinedVariableException Thrown when a variable is undefined.
      */
-    public boolean updateUserBirthdayMention(final String userID, final boolean alreadyMentioned)
-    throws AuthorizationException, ResponseException, NotFoundException, UndefinedVariableException {
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.PATCH)
+    public CompletableFuture<Boolean> updateUserBirthdayMention(final String userID, final boolean alreadyMentioned) {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.PATCH)
                 .setRoute("/birthdays/" + userID + "/mention")
                 .addParameter("already_mentioned", String.valueOf(alreadyMentioned))
                 .setAuthorization(apiKey)
-                .build().orElseThrow();
-
-        return request.getStatusCode() == 200;
-    }
-
-    public void updateUserBirthdayMentionAsync(final String userID, final boolean alreadyMentioned, Runnable successFunction, Consumer<Exception> errorFunction) {
-        RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.PATCH)
-                .setRoute("/birthdays/" + userID + "/mention")
-                .addParameter("already_mentioned", String.valueOf(alreadyMentioned))
-                .setAuthorization(apiKey)
-                .onSuccess((request) -> successFunction.run())
-                .onError(errorFunction)
-                .buildAsync();
-    }
-
-    public void updateUserBirthdayMentionAsync(final String userID, final boolean alreadyMentioned, Runnable successFunction) {
-        RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.PATCH)
-                .setRoute("/birthdays/" + userID + "/mention")
-                .addParameter("already_mentioned", String.valueOf(alreadyMentioned))
-                .setAuthorization(apiKey)
-                .buildAsync((request) -> successFunction.run());
+                .buildAsync()
+                .thenApplyAsync((optionalRequest) -> {
+                    if (optionalRequest.isPresent()) return optionalRequest.get().getStatusCode() == 200;
+                    throw new CompletionException("Error updating user mention. Request is empty.", null);
+                });
     }
 
     /**
@@ -206,93 +110,37 @@ public class BirthdaysEndpoint extends CafeEndpoint {
      * @param userID The specified {@link String userID}.
      * @param birthday The {@link Birthday} specified for the {@link String userID}.
      * @return True, if the {@link Birthday} was successfully created.
-     * @throws AuthorizationException Thrown when the {@link String apiKey} is invalid.
-     * @throws ResponseException Thrown when there is a generic server-side {@link CafeException}.
-     * @throws ConflictException Thrown when the {@link Birthday} for the specified {@link String userID} already exists.
-     * @throws UndefinedVariableException Thrown when a variable is undefined.
-     * @throws TeaPotException Thrown when the {@link BirthdayMonth month} or {@link Integer day} is invalid.
      */
-    public boolean createUserBirthday(final String userID, final Birthday birthday)
-    throws AuthorizationException, ResponseException, ConflictException, UndefinedVariableException, TeaPotException {
+    public CompletableFuture<Boolean> createUserBirthday(final String userID, final Birthday birthday) {
         BirthdayMonth month = birthday.getMonth();
         int day = birthday.getDay();
 
-        // TODO: Check if this error checking is required.
-//        // Checking for days in a month.
-//        if (month.getDaysInMonth() < day)
-//            throw new TeaPotException("There are only " + month.getDaysInMonth() + " days in " + month + ", not " + day + ".");
-//
-//        // Checking if the month is valid.
-//        if (month == BirthdayMonth.ERROR)
-//            throw new TeaPotException(month + " is an invalid month.");
-
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.POST)
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.POST)
                 .setRoute("/birthdays/" + userID)
                 .addParameter("birthday", getBirthdayString(month, day))
                 .addParameter("time_zone", birthday.getTimeZone().getID())
                 .setAuthorization(apiKey)
-                .build().orElseThrow();
-
-        return request.getStatusCode() == 201;
-    }
-
-    public void createUserBirthdayAsync(final String userID, final Birthday birthday, final Runnable successFunction, final Consumer<Exception> errorFunction) {
-        BirthdayMonth month = birthday.getMonth();
-        int day = birthday.getDay();
-
-        RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.POST)
-                .setRoute("/birthdays/" + userID)
-                .addParameter("birthday", getBirthdayString(month, day))
-                .addParameter("time_zone", birthday.getTimeZone().getID())
-                .setAuthorization(apiKey)
-                .onSuccess((request) -> successFunction.run())
-                .onError(errorFunction)
-                .buildAsync();
-    }
-
-    public void createUserBirthdayAsync(final String userID, final Birthday birthday, final Runnable successFunction) {
-        BirthdayMonth month = birthday.getMonth();
-        int day = birthday.getDay();
-
-        RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.POST)
-                .setRoute("/birthdays/" + userID)
-                .addParameter("birthday", getBirthdayString(month, day))
-                .addParameter("time_zone", birthday.getTimeZone().getID())
-                .setAuthorization(apiKey)
-                .buildAsync((request) -> successFunction.run());
+                .buildAsync()
+                .thenApplyAsync((optionalRequest) -> {
+                    if (optionalRequest.isPresent()) return optionalRequest.get().getStatusCode() == 201;
+                    throw new CompletionException("Error creating user birthday. Request is empty.", null);
+                });
     }
 
     /**
      * Removes a {@link Birthday} for a specified {@link String userID}.
      * @param userID The specified {@link String userID}.
      * @return True, if the {@link Birthday} has been successfully removed from the {@link CafeAPI CafeAPI}.
-     * @throws AuthorizationException Thrown when the {@link String apiKey} is invalid.
-     * @throws ResponseException Thrown when there is a generic server-side {@link CafeException}.
      */
-    public boolean removeUserBirthday(final String userID)
-    throws AuthorizationException, ResponseException {
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.DELETE)
+    public CompletableFuture<Boolean> removeUserBirthday(final String userID) {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.DELETE)
                 .setRoute("/birthdays/" + userID)
                 .setAuthorization(apiKey)
-                .build().orElseThrow();
-
-        return request.getStatusCode() == 200;
-    }
-
-    public void removeUserBirthdayAsync(final String userID, final Runnable successFunction, final Consumer<Exception> errorFunction) {
-        RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.DELETE)
-                .setRoute("/birthdays/" + userID)
-                .setAuthorization(apiKey)
-                .onSuccess((request) -> successFunction.run())
-                .onError(errorFunction)
-                .buildAsync();
-    }
-
-    public void removeUserBirthdayAsync(final String userID, final Runnable successFunction) {
-        RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.DELETE)
-                .setRoute("/birthdays/" + userID)
-                .setAuthorization(apiKey)
-                .buildAsync((request) -> successFunction.run());
+                .buildAsync()
+                .thenApplyAsync((optionalRequest) -> {
+                    if (optionalRequest.isPresent()) return optionalRequest.get().getStatusCode() == 200;
+                    throw new CompletionException("Error removing user birthday. Request is empty.", null);
+                });
     }
 
     /**
