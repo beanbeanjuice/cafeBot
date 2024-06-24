c17161339d6e874d71303269634222c3c06e648c
beanbeanjuice <beanbeanjuice@outlook.com>
2024-06-24 06:01:33 -0400

    Added Async to Raffles Endpoint

--- wrappers/cafe-api-wrapper/src/main/java/com/beanbeanjuice/cafeapi/wrapper/endpoints/raffles/RafflesEndpoint.java
+++ wrappers/cafe-api-wrapper/src/main/java/com/beanbeanjuice/cafeapi/wrapper/endpoints/raffles/RafflesEndpoint.java
@@ -1,140 +1,83 @@
 package com.beanbeanjuice.cafeapi.wrapper.endpoints.raffles;
 
-import com.beanbeanjuice.cafeapi.wrapper.CafeAPI;
 import com.beanbeanjuice.cafeapi.wrapper.endpoints.CafeEndpoint;
 import com.beanbeanjuice.cafeapi.wrapper.generic.CafeGeneric;
-import com.beanbeanjuice.cafeapi.wrapper.requests.Request;
 import com.beanbeanjuice.cafeapi.wrapper.requests.RequestBuilder;
 import com.beanbeanjuice.cafeapi.wrapper.requests.RequestRoute;
 import com.beanbeanjuice.cafeapi.wrapper.requests.RequestType;
-import com.beanbeanjuice.cafeapi.wrapper.exception.api.AuthorizationException;
-import com.beanbeanjuice.cafeapi.wrapper.exception.api.ConflictException;
-import com.beanbeanjuice.cafeapi.wrapper.exception.api.ResponseException;
-import com.beanbeanjuice.cafeapi.wrapper.exception.api.UndefinedVariableException;
-import com.beanbeanjuice.cafeapi.wrapper.exception.api.CafeException;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
 
-/**
- * A class used for requests about {@link RafflesEndpoint} in the {@link CafeAPI}.
- *
- * @author beanbeanjuice
- */
 public class RafflesEndpoint extends CafeEndpoint {
 
-    /**
-     * Retrieves all {@link Raffle Raffles} in the {@link CafeAPI CafeAPI}.
-     * @return The {@link HashMap} with keys of {@link String guildID} and a value of {@link ArrayList} containing {@link Raffle Raffles}.
-     * @throws AuthorizationException Thrown when the {@link String apiKey} is invalid.
-     * @throws ResponseException Thrown when there is a generic server-side {@link CafeException CafeException}.
-     */
-    public HashMap<String, ArrayList<Raffle>> getAllRaffles()
-    throws AuthorizationException, ResponseException {
-        HashMap<String, ArrayList<Raffle>> raffles = new HashMap<>();
-
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
+    public CompletableFuture<HashMap<String, ArrayList<Raffle>>> getAllRaffles() {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
                 .setRoute("/raffles")
                 .setAuthorization(apiKey)
-                .build();
+                .buildAsync()
+                .thenApplyAsync((request) -> {
+                    HashMap<String, ArrayList<Raffle>> raffles = new HashMap<>();
 
-        request.getData().get("raffles").forEach((raffle) -> {
-            String guildID = raffle.get("guild_id").asText();
-            String messageID = raffle.get("message_id").asText();
-            Timestamp endingTime = CafeGeneric.parseTimestampFromAPI(raffle.get("ending_time").asText()).orElse(null);
-            Integer winnerAmount = raffle.get("winner_amount").asInt();
+                    request.getData().get("raffles").forEach((raffle) -> {
+                        String guildID = raffle.get("guild_id").asText();
+                        String messageID = raffle.get("message_id").asText();
+                        Timestamp endingTime = CafeGeneric.parseTimestampFromAPI(raffle.get("ending_time").asText()).orElse(null);
+                        int winnerAmount = raffle.get("winner_amount").asInt();
 
-            if (!raffles.containsKey(guildID)) raffles.put(guildID, new ArrayList<>());
+                        if (!raffles.containsKey(guildID)) raffles.put(guildID, new ArrayList<>());
 
-            raffles.get(guildID).add(new Raffle(messageID, endingTime, winnerAmount));
-        });
+                        raffles.get(guildID).add(new Raffle(messageID, endingTime, winnerAmount));
+                    });
 
-        return raffles;
+                    return raffles;
+                });
     }
 
-    /**
-     * Retrieves all {@link Raffle Raffles} from a specified Discord server.
-     * @param guildID The {@link String guildID} of the Discord server.
-     * @return The {@link ArrayList} of {@link Raffle} in the {@link CafeAPI CafeAPI}.
-     * @throws AuthorizationException Thrown when the {@link String apiKey} is invalid.
-     * @throws ResponseException Thrown when there is a generic server-side {@link CafeException CafeException}.
-     */
-    public ArrayList<Raffle> getGuildRaffles(final String guildID)
-    throws AuthorizationException, ResponseException {
-        ArrayList<Raffle> raffles = new ArrayList<>();
-
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
+    public CompletableFuture<ArrayList<Raffle>> getGuildRaffles(final String guildID) {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
                 .setRoute("/raffles/" + guildID)
                 .setAuthorization(apiKey)
-                .build();
+                .buildAsync()
+                .thenApplyAsync((request) -> {
+                    ArrayList<Raffle> raffles = new ArrayList<>();
 
-        request.getData().get("raffles").forEach((raffle) -> {
-            String messageID = raffle.get("message_id").asText();
-            Timestamp endingTime = CafeGeneric.parseTimestampFromAPI(raffle.get("ending_time").asText()).orElse(null);
-            Integer winnerAmount = raffle.get("winner_amount").asInt();
+                    request.getData().get("raffles").forEach((raffle) -> {
+                        String messageID = raffle.get("message_id").asText();
+                        Timestamp endingTime = CafeGeneric.parseTimestampFromAPI(raffle.get("ending_time").asText()).orElse(null);
+                        int winnerAmount = raffle.get("winner_amount").asInt();
 
-            raffles.add(new Raffle(messageID, endingTime, winnerAmount));
-        });
+                        raffles.add(new Raffle(messageID, endingTime, winnerAmount));
+                    });
 
-        return raffles;
+                    return raffles;
+                });
     }
 
-    /**
-     * Creates a new {@link Raffle} in the {@link CafeAPI CafeAPI}.
-     * @param guildID The {@link String guildID} of the {@link Raffle}.
-     * @param raffle The {@link Raffle} to add to the {@link CafeAPI CafeAPI}.
-     * @return True, if the {@link Raffle} was successfully created.
-     * @throws AuthorizationException Thrown when the {@link String apiKey} is invalid.
-     * @throws ResponseException Thrown when there is a generic server-side {@link CafeException CafeException}.
-     * @throws UndefinedVariableException Thrown when a variable is undefined.
-     * @throws ConflictException Thrown when the {@link Raffle} already exists for the specified {@link String guildID}.
-     */
-    public boolean createRaffle(final String guildID, final Raffle raffle)
-    throws AuthorizationException, ResponseException, UndefinedVariableException, ConflictException {
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.POST)
+    public CompletableFuture<Boolean> createRaffle(final String guildID, final Raffle raffle) {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.POST)
                 .setRoute("/raffles/" + guildID)
                 .addParameter("message_id", raffle.getMessageID())
                 .addParameter("ending_time", raffle.getEndingTime().toString())
                 .addParameter("winner_amount", String.valueOf(raffle.getWinnerAmount()))
                 .setAuthorization(apiKey)
-                .build();
-
-        return request.getStatusCode() == 201;
+                .buildAsync()
+                .thenApplyAsync((request) -> request.getStatusCode() == 201);
     }
 
-    /**
-     * Deletes a {@link Raffle} from the {@link CafeAPI CafeAPI}.
-     * @param guildID The {@link String guildID} of the {@link Raffle}.
-     * @param raffle The {@link Raffle} to delete from the {@link CafeAPI CafeAPI}.
-     * @return True, if the {@link Raffle} was successfully removed from the {@link CafeAPI CafeAPI}.
-     * @throws AuthorizationException Thrown when the {@link String apiKey} is invalid.
-     * @throws ResponseException Thrown when there is a generic server-side {@link CafeException CafeException}.
-     * @throws UndefinedVariableException Thrown when a variable is undefined.
-     */
-    public boolean deleteRaffle(final String guildID, final Raffle raffle)
-    throws AuthorizationException, ResponseException, UndefinedVariableException {
+    public CompletableFuture<Boolean> deleteRaffle(final String guildID, final Raffle raffle) {
         return deleteRaffle(guildID, raffle.getMessageID());
     }
 
-    /**
-     * Deletes a {@link Raffle} from the {@link CafeAPI CafeAPI}.
-     * @param guildID The {@link String guildID} of the {@link Raffle}.
-     * @param messageID The {@link String messageID} of the {@link Raffle}.
-     * @return True, if the {@link Raffle} was successfully removed from the {@link CafeAPI CafeAPI}.
-     * @throws AuthorizationException Thrown when the {@link String apiKey} is invalid.
-     * @throws ResponseException Thrown when there is a generic server-side {@link CafeException CafeException}.
-     * @throws UndefinedVariableException Thrown when a variable is undefined.
-     */
-    public boolean deleteRaffle(final String guildID, final String messageID)
-    throws AuthorizationException, ResponseException, UndefinedVariableException {
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.DELETE)
+    public CompletableFuture<Boolean> deleteRaffle(final String guildID, final String messageID) {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.DELETE)
                 .setRoute("/raffles/" + guildID)
                 .addParameter("message_id", messageID)
                 .setAuthorization(apiKey)
-                .build();
-
-        return request.getStatusCode() == 200;
+                .buildAsync()
+                .thenApplyAsync((request) -> request.getStatusCode() == 200);
     }
 
 }
--- wrappers/cafe-api-wrapper/src/test/java/com/beanbeanjuice/cafeapi/beta/RafflesEndpointTests.java
+++ wrappers/cafe-api-wrapper/src/test/java/com/beanbeanjuice/cafeapi/beta/RafflesEndpointTests.java
@@ -11,37 +11,47 @@ import org.junit.jupiter.api.Test;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
-public class RaffleTest {
+public class RafflesEndpointTests {
 
     @Test
     @DisplayName("Raffles Endpoint Test")
-    public void testRafflesEndpoint() {
+    public void testRafflesEndpoint() throws ExecutionException, InterruptedException {
         CafeAPI cafeAPI = new CafeAPI("beanbeanjuice", System.getenv("API_PASSWORD"), RequestLocation.BETA);
 
         long currentTime = System.currentTimeMillis();
         Timestamp currentTimestamp = CafeGeneric.parseTimestamp(new Timestamp(currentTime).toString()).orElse(null);
 
         // Makes sure the raffle is deleted beforehand.
-        Assertions.assertTrue(cafeAPI.getRafflesEndpoint().deleteRaffle("798830792938881024", "878895791081676831"));
+        Assertions.assertTrue(cafeAPI.getRafflesEndpoint().deleteRaffle("798830792938881024", "878895791081676831").get());
 
         // Makes sure a raffle can be created.
-        Assertions.assertTrue(() -> cafeAPI.getRafflesEndpoint().createRaffle("798830792938881024", new Raffle(
+        Assertions.assertTrue(cafeAPI.getRafflesEndpoint().createRaffle("798830792938881024", new Raffle(
                 "878895791081676831",
                 currentTimestamp,
                 3
-        )));
+        )).get());
 
         // Makes sure creating the same raffle throws an exception.
-        Assertions.assertThrows(ConflictException.class, () -> cafeAPI.getRafflesEndpoint().createRaffle("798830792938881024", new Raffle(
-                "878895791081676831",
-                currentTimestamp,
-                3
-        )));
+        cafeAPI.getRafflesEndpoint().createRaffle("798830792938881024", new Raffle(
+                        "878895791081676831",
+                        currentTimestamp,
+                        3
+                )).thenAcceptAsync((isSuccess) -> Assertions.fail())
+                .exceptionallyAsync((exception) -> {
+                    Assertions.assertInstanceOf(ConflictException.class, exception.getCause());
+                    return null;
+                }).join();
 
         // Makes sure that the newly created raffle shows up in the API.
         Assertions.assertTrue(() -> {
-            ArrayList<Raffle> raffles = cafeAPI.getRafflesEndpoint().getAllRaffles().get("798830792938881024");
+            ArrayList<Raffle> raffles = null;
+            try {
+                raffles = cafeAPI.getRafflesEndpoint().getAllRaffles().get().get("798830792938881024");
+            } catch (Exception e) {
+                Assertions.fail(e);
+            }
 
             for (Raffle raffle : raffles) {
                 if (raffle.getMessageID().equals("878895791081676831")) {
@@ -54,7 +64,12 @@ public class RaffleTest {
 
         // Makes sure that retrieving the timestamp of the poll works.
         Assertions.assertTrue(() -> {
-            ArrayList<Raffle> raffles = cafeAPI.getRafflesEndpoint().getGuildRaffles("798830792938881024");
+            ArrayList<Raffle> raffles = null;
+            try {
+                raffles = cafeAPI.getRafflesEndpoint().getGuildRaffles("798830792938881024").get();
+            } catch (Exception e) {
+                Assertions.fail(e);
+            }
 
             for (Raffle raffle : raffles) {
                 if (raffle.getEndingTime().equals(currentTimestamp)) {
@@ -67,7 +82,12 @@ public class RaffleTest {
 
         // Makes sure that getting the raffle directly also works.
         Assertions.assertTrue(() -> {
-            ArrayList<Raffle> raffles = cafeAPI.getRafflesEndpoint().getGuildRaffles("798830792938881024");
+            ArrayList<Raffle> raffles = null;
+            try {
+                raffles = cafeAPI.getRafflesEndpoint().getGuildRaffles("798830792938881024").get();
+            } catch (Exception e) {
+                Assertions.fail(e);
+            }
 
             for (Raffle raffle : raffles) {
                 if (raffle.getMessageID().equals("878895791081676831")) {
@@ -79,11 +99,16 @@ public class RaffleTest {
         });
 
         // Makes sure deleting the raffle is able to happen.
-        Assertions.assertTrue(cafeAPI.getRafflesEndpoint().deleteRaffle("798830792938881024", "878895791081676831"));
+        Assertions.assertTrue(cafeAPI.getRafflesEndpoint().deleteRaffle("798830792938881024", "878895791081676831").get());
 
         // Makes sure the newly created raffle that was deleted no longer shows up in the API.
         Assertions.assertFalse(() -> {
-            ArrayList<Raffle> raffles = cafeAPI.getRafflesEndpoint().getAllRaffles().get("798830792938881024");
+            ArrayList<Raffle> raffles = null;
+            try {
+                raffles = cafeAPI.getRafflesEndpoint().getAllRaffles().get().get("798830792938881024");
+            } catch (Exception e) {
+                Assertions.fail(e);
+            }
 
             if (raffles == null) {
                 return false;
