a90d0b1f51499cc4b17bb6abdd0ebc65ac901f8c
beanbeanjuice <beanbeanjuice@outlook.com>
2024-06-24 05:14:46 -0400

    Added Async to Goodbyes Endpoint

--- wrappers/cafe-api-wrapper/src/main/java/com/beanbeanjuice/cafeapi/wrapper/endpoints/goodbyes/GoodbyesEndpoint.java
+++ wrappers/cafe-api-wrapper/src/main/java/com/beanbeanjuice/cafeapi/wrapper/endpoints/goodbyes/GoodbyesEndpoint.java
@@ -1,127 +1,68 @@
 package com.beanbeanjuice.cafeapi.wrapper.endpoints.goodbyes;
 
-import com.beanbeanjuice.cafeapi.wrapper.CafeAPI;
 import com.beanbeanjuice.cafeapi.wrapper.endpoints.CafeEndpoint;
-import com.beanbeanjuice.cafeapi.wrapper.requests.Request;
 import com.beanbeanjuice.cafeapi.wrapper.requests.RequestBuilder;
 import com.beanbeanjuice.cafeapi.wrapper.requests.RequestRoute;
 import com.beanbeanjuice.cafeapi.wrapper.requests.RequestType;
-import com.beanbeanjuice.cafeapi.wrapper.exception.api.*;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
 
-/**
- * A class used for the {@link GoodbyesEndpoint} API.
- */
 public class GoodbyesEndpoint extends CafeEndpoint {
 
-    /**
-     * Retrieves an {@link ArrayList} of {@link GuildGoodbye} containing all Guild Goodbyes in the {@link CafeAPI CafeAPI}.
-     * @return The {@link ArrayList} of {@link GuildGoodbye}.
-     * @throws AuthorizationException Thrown when the api key is unauthorized.
-     * @throws ResponseException Thrown when there is a generic server-side exception.
-     */
-    public ArrayList<GuildGoodbye> getAllGuildGoodbyes() throws AuthorizationException, ResponseException {
-        ArrayList<GuildGoodbye> guildGoodbyes = new ArrayList<>();
-
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
+    public CompletableFuture<ArrayList<GuildGoodbye>> getAllGuildGoodbyes() {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
                 .setRoute("/goodbyes")
                 .setAuthorization(apiKey)
-                .build();
-
-        request.getData().get("goodbyes").forEach((guildGoodbye) -> guildGoodbyes.add(parseGuildGoodbye(guildGoodbye)));
-
-        return guildGoodbyes;
+                .buildAsync()
+                .thenApply((request) -> {
+                    ArrayList<GuildGoodbye> guildGoodbyes = new ArrayList<>();
+                    request.getData().get("goodbyes").forEach((guildGoodbye) -> guildGoodbyes.add(parseGuildGoodbye(guildGoodbye)));
+                    return guildGoodbyes;
+                });
     }
 
-    /**
-     * Retrieves a {@link GuildGoodbye} from the {@link CafeAPI CafeAPI}.
-     * @param guildID The {@link String guildID} to retrieve the {@link GuildGoodbye} for.
-     * @return The {@link GuildGoodbye} retrieved.
-     * @throws AuthorizationException Thrown when the API key is invalid.
-     * @throws ResponseException Thrown when there is a generic server-side exception.
-     * @throws NotFoundException Thrown when the guild ID is not found.
-     */
-    public GuildGoodbye getGuildGoodbye(final String guildID)
-            throws AuthorizationException, ResponseException, NotFoundException {
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
+    public CompletableFuture<GuildGoodbye> getGuildGoodbye(final String guildID) {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
                 .setRoute("/goodbyes/" + guildID)
                 .setAuthorization(apiKey)
-                .build();
-
-        JsonNode guildGoodbye = request.getData().get("goodbye");
-
-        return parseGuildGoodbye(guildGoodbye);
+                .buildAsync()
+                .thenApply((request) -> parseGuildGoodbye(request.getData().get("goodbye")));
     }
 
-    /**
-     * Updates a {@link GuildGoodbye} in the {@link CafeAPI CafeAPI}.
-     * @param guildGoodbye The new {@link GuildGoodbye}.
-     * @return True, if updating the {@link GuildGoodbye} was successful.
-     * @throws AuthorizationException Thrown when the API key is invalid.
-     * @throws NotFoundException Thrown when the guild ID is not found.
-     * @throws ResponseException Thrown when there is a generic server-side exception.
-     */
-    public Boolean updateGuildGoodbye(final GuildGoodbye guildGoodbye)
-            throws AuthorizationException, NotFoundException, ResponseException {
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.PATCH)
+    public CompletableFuture<Boolean> updateGuildGoodbye(final GuildGoodbye guildGoodbye) {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.PATCH)
                 .setRoute("/goodbyes/" + guildGoodbye.getGuildID())
                 .addParameter("description", guildGoodbye.getDescription().orElse(null))
                 .addParameter("thumbnail_url", guildGoodbye.getThumbnailURL().orElse(null))
                 .addParameter("image_url", guildGoodbye.getImageURL().orElse(null))
                 .addParameter("message", guildGoodbye.getMessage().orElse(null))
                 .setAuthorization(apiKey)
-                .build();
-
-        return request.getStatusCode() == 200;
+                .buildAsync()
+                .thenApply((request) -> request.getStatusCode() == 200);
     }
 
-    /**
-     * Creates a new {@link GuildGoodbye} for the {@link CafeAPI CafeAPI}.
-     * @param guildGoodbye The new {@link GuildGoodbye} to add.
-     * @return True if the {@link GuildGoodbye} was successfully added.
-     * @throws AuthorizationException Thrown when the API key is invalid.
-     * @throws ConflictException Thrown when the provided guild ID already exists.
-     * @throws ResponseException Thrown when there is a generic server-side exception.
-     * @throws UndefinedVariableException Thrown when a variable is undefined.
-     */
-    public Boolean createGuildGoodbye(final GuildGoodbye guildGoodbye)
-            throws AuthorizationException, ConflictException, ResponseException, UndefinedVariableException {
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.POST)
+    public CompletableFuture<Boolean> createGuildGoodbye(final GuildGoodbye guildGoodbye) {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.POST)
                 .setRoute("/goodbyes/" + guildGoodbye.getGuildID())
                 .addParameter("description", guildGoodbye.getDescription().orElse(null))
                 .addParameter("thumbnail_url", guildGoodbye.getThumbnailURL().orElse(null))
                 .addParameter("image_url", guildGoodbye.getImageURL().orElse(null))
                 .addParameter("message", guildGoodbye.getMessage().orElse(null))
                 .setAuthorization(apiKey)
-                .build();
-
-        return request.getStatusCode() == 201;
+                .buildAsync()
+                .thenApply((request) -> request.getStatusCode() == 201);
     }
 
-    /**
-     * Deletes a {@link GuildGoodbye} from the {@link CafeAPI CafeAPI}.
-     * @param guildID The {@link String} ID of the {@link GuildGoodbye} to delete.
-     * @return True if successfully deleted.
-     * @throws AuthorizationException Thrown when the API key is invalid.
-     * @throws ResponseException Thrown when there is a generic server-side exception.
-     */
-    public Boolean deleteGuildGoodbye(final String guildID)
-            throws AuthorizationException, ResponseException {
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.DELETE)
+    public CompletableFuture<Boolean> deleteGuildGoodbye(final String guildID) {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.DELETE)
                 .setRoute("/goodbyes/" + guildID)
                 .setAuthorization(apiKey)
-                .build();
-
-        return request.getStatusCode() == 200;
+                .buildAsync()
+                .thenApply((request) -> request.getStatusCode() == 200);
     }
 
-    /**
-     * Parses a {@link GuildGoodbye} from the {@link JsonNode}.
-     * @param node The {@link JsonNode} to parse.
-     * @return The parsed {@link GuildGoodbye}.
-     */
     private GuildGoodbye parseGuildGoodbye(final JsonNode node) {
         String guildID = node.get("guild_id").asText();
 
--- wrappers/cafe-api-wrapper/src/test/java/com/beanbeanjuice/cafeapi/beta/GoodbyesEndpointTests.java
+++ wrappers/cafe-api-wrapper/src/test/java/com/beanbeanjuice/cafeapi/beta/GoodbyesEndpointTests.java
@@ -10,20 +10,22 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ExecutionException;
+
 /**
  * A test class used to test all aspects of the {@link GoodbyesEndpoint} module.
  *
  * @author beanbeanjuice
  */
-public class GuildGoodbyeTest {
+public class GoodbyesEndpointTests {
 
     @Test
     @DisplayName("Goodbyes Endpoint Test")
-    public void testGoodbyesEndpoint() {
+    public void testGoodbyesEndpoint() throws ExecutionException, InterruptedException {
         CafeAPI cafeAPI = new CafeAPI("beanbeanjuice", System.getenv("API_PASSWORD"), RequestLocation.BETA);
 
         // Makes sure that the amount of guilds in the guild's goodbye is more than 0.
-        Assertions.assertTrue(cafeAPI.getGoodbyesEndpoint().getAllGuildGoodbyes().size() > 0);
+        Assertions.assertFalse(cafeAPI.getGoodbyesEndpoint().getAllGuildGoodbyes().get().isEmpty());
 
         // Makes sure that the current goodbye is set properly.
         Assertions.assertTrue(cafeAPI.getGoodbyesEndpoint().updateGuildGoodbye(new GuildGoodbye(
@@ -32,24 +34,32 @@ public class GuildGoodbyeTest {
                 "https://i.pinimg.com/originals/3f/33/75/3f3375eaef9ed7529d0e1bb5b63a814a.gif",
                 "https://i.pinimg.com/originals/3f/33/75/3f3375eaef9ed7529d0e1bb5b63a814a.gif",
                 null
-        )));
+        )).get());
 
         // Makes sure that the first guild is equal to the home guild ID.
-        Assertions.assertEquals("798830792938881024", cafeAPI.getGoodbyesEndpoint().getAllGuildGoodbyes().get(0).getGuildID());
+        Assertions.assertEquals("798830792938881024", cafeAPI.getGoodbyesEndpoint().getAllGuildGoodbyes().get().getFirst().getGuildID());
 
         // Makes sure that the description of the retrieved guild is "test description"
         Assertions.assertEquals(
                 "Goodbye... {user}... thanks for joining!",
-                cafeAPI.getGoodbyesEndpoint().getGuildGoodbye("798830792938881024").getDescription().orElse(null));
+                cafeAPI.getGoodbyesEndpoint().getGuildGoodbye("798830792938881024").get().getDescription().orElse(null));
 
         // Makes sure that this throws a NotFoundException as the guild "bruhmoment" does not exist.
-        Assertions.assertThrows(NotFoundException.class, () -> {
-            cafeAPI.getGoodbyesEndpoint().getGuildGoodbye("bruhmoment");
-        });
+        try {
+            cafeAPI.getGoodbyesEndpoint().getGuildGoodbye("bruhmoment").get();
+            Assertions.fail();
+        } catch (Exception e) {
+            Assertions.assertInstanceOf(NotFoundException.class, e.getCause());
+        }
 
         // Makes sure that the guild can be updated.
         Assertions.assertTrue(() -> {
-            GuildGoodbye currentGuildGoodbye = cafeAPI.getGoodbyesEndpoint().getGuildGoodbye("798830792938881024");
+            GuildGoodbye currentGuildGoodbye = null;
+            try {
+                currentGuildGoodbye = cafeAPI.getGoodbyesEndpoint().getGuildGoodbye("798830792938881024").get();
+            } catch (Exception e) {
+                Assertions.fail();
+            }
 
             GuildGoodbye newGuildGoodbye = new GuildGoodbye(
                     currentGuildGoodbye.getGuildID(),
@@ -59,11 +69,16 @@ public class GuildGoodbyeTest {
                     "cool message!!!!"
             );
 
-            return cafeAPI.getGoodbyesEndpoint().updateGuildGoodbye(newGuildGoodbye);
+            try {
+                return cafeAPI.getGoodbyesEndpoint().updateGuildGoodbye(newGuildGoodbye).get();
+            } catch (Exception e) {
+                Assertions.fail();
+                return false;
+            }
         });
 
         // Throws a conflict exception because the guild goodbye cannot be created because it already exists.
-        Assertions.assertThrows(ConflictException.class, () -> {
+        try {
             GuildGoodbye guildGoodbye = new GuildGoodbye(
                     "798830792938881024",
                     "COOL desc",
@@ -72,8 +87,11 @@ public class GuildGoodbyeTest {
                     null
             );
 
-            cafeAPI.getGoodbyesEndpoint().createGuildGoodbye(guildGoodbye);
-        });
+            cafeAPI.getGoodbyesEndpoint().createGuildGoodbye(guildGoodbye).get();
+            Assertions.fail();
+        } catch (Exception e) {
+            Assertions.assertInstanceOf(ConflictException.class, e.getCause());
+        }
 
         // Makes sure that a guild can be created.
         Assertions.assertTrue(() -> {
@@ -85,11 +103,16 @@ public class GuildGoodbyeTest {
                     null
             );
 
-            return cafeAPI.getGoodbyesEndpoint().createGuildGoodbye(guildGoodbye);
+            try {
+                return cafeAPI.getGoodbyesEndpoint().createGuildGoodbye(guildGoodbye).get();
+            } catch (Exception e) {
+                Assertions.fail();
+                return false;
+            }
         });
 
         // Is true when a guild goodbye is successfully deleted.
-        Assertions.assertTrue(cafeAPI.getGoodbyesEndpoint().deleteGuildGoodbye("236331890964037632"));
+        Assertions.assertTrue(cafeAPI.getGoodbyesEndpoint().deleteGuildGoodbye("236331890964037632").get());
     }
 
 }
