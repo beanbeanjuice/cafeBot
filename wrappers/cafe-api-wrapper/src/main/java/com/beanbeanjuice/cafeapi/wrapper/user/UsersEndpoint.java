e6376007f7ecb08775e4866b625cca3aa0a93a78
beanbeanjuice <beanbeanjuice@outlook.com>
2024-06-24 06:40:18 -0400

    Added Async to Users Endpoint

--- wrappers/cafe-api-wrapper/src/main/java/com/beanbeanjuice/cafeapi/wrapper/user/UsersEndpoint.java
+++ wrappers/cafe-api-wrapper/src/main/java/com/beanbeanjuice/cafeapi/wrapper/user/UsersEndpoint.java
@@ -1,89 +1,61 @@
 package com.beanbeanjuice.cafeapi.wrapper.user;
 
 import com.beanbeanjuice.cafeapi.wrapper.endpoints.CafeEndpoint;
-import com.beanbeanjuice.cafeapi.wrapper.exception.api.AuthorizationException;
-import com.beanbeanjuice.cafeapi.wrapper.exception.api.ResponseException;
-import com.beanbeanjuice.cafeapi.wrapper.exception.api.CafeException;
-import com.beanbeanjuice.cafeapi.wrapper.requests.Request;
 import com.beanbeanjuice.cafeapi.wrapper.requests.RequestBuilder;
 import com.beanbeanjuice.cafeapi.wrapper.requests.RequestRoute;
 import com.beanbeanjuice.cafeapi.wrapper.requests.RequestType;
 
 import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
 
-/**
- * A class used for everything to do with {@link UsersEndpoint} in the API.
- *
- * @author beanbeanjuice
- */
 public class UsersEndpoint extends CafeEndpoint {
 
-    /**
-     * @return The {@link ArrayList} of {@link User users} in the API database.
-     * @throws AuthorizationException Thrown when the logged in account does not have access to view all users.
-     */
-    public ArrayList<User> getUsers() throws AuthorizationException, ResponseException {
-        ArrayList<User> users = new ArrayList<>();
-
-        Request request = RequestBuilder.create(RequestRoute.CAFE, RequestType.GET)
+    public CompletableFuture<ArrayList<User>> getUsers() {
+        return RequestBuilder.create(RequestRoute.CAFE, RequestType.GET)
                 .setRoute("/users")
                 .setAuthorization(apiKey)
-                .build();
+                .buildAsync()
+                .thenApplyAsync((request) -> {
+                    ArrayList<User> users = new ArrayList<>();
 
-        request.getData().get("users").forEach(user -> {
-            int id = user.get("user_id").intValue();
-            String username = user.get("username").textValue();
-            UserType userType = UserType.valueOf(user.get("user_type").textValue());
-            users.add(new User(id, username, userType));
-        });
+                    request.getData().get("users").forEach(user -> {
+                        int id = user.get("user_id").intValue();
+                        String username = user.get("username").textValue();
+                        UserType userType = UserType.valueOf(user.get("user_type").textValue());
+                        users.add(new User(id, username, userType));
+                    });
 
-        return users;
+                    return users;
+                });
     }
 
-    /**
-     * Signup a {@link User}.
-     * @param username The {@link String username} of the {@link User}.
-     * @param password The {@link String password} of the {@link User}.
-     * @return True, if successfully signed up.
-     */
-    public boolean signUp(final String username, final String password) {
-        Request request = RequestBuilder.create(RequestRoute.CAFE, RequestType.POST)
+    public CompletableFuture<Boolean> signUp(final String username, final String password) {
+        return RequestBuilder.create(RequestRoute.CAFE, RequestType.POST)
                 .setRoute("/user/signup")
                 .addParameter("username", username)
                 .addParameter("password", password)
-                .build();
-
-        return request.getStatusCode() == 201;
+                .buildAsync()
+                .thenApplyAsync((request) -> request.getStatusCode() == 201);
     }
 
-    /**
-     * Retrieves a {@link User} from the {@link com.beanbeanjuice.cafeapi.wrapper.CafeAPI CafeAPI}.
-     * @param username The {@link String username} of the {@link User}.
-     * @return The specified {@link User}.
-     * @throws AuthorizationException Thrown when the {@link String apiKey} is invalid.
-     * @throws ResponseException Thrown when there is a generic server-side {@link CafeException CafeException}.
-     */
-    public User getUser(final String username) throws AuthorizationException, ResponseException {
-        Request request = RequestBuilder.create(RequestRoute.CAFE, RequestType.GET)
+    public CompletableFuture<User> getUser(final String username) {
+        return RequestBuilder.create(RequestRoute.CAFE, RequestType.GET)
                 .setRoute("/user/" + username)
                 .setAuthorization(apiKey)
-                .build();
-
-        int ID = request.getData().get("user").get("user_id").intValue();
-        UserType userType = UserType.valueOf(request.getData().get("user").get("user_type").textValue());
-        return new User(ID, username, userType);
+                .buildAsync()
+                .thenApplyAsync((request) -> {
+                    int ID = request.getData().get("user").get("user_id").intValue();
+                    UserType userType = UserType.valueOf(request.getData().get("user").get("user_type").textValue());
+                    return new User(ID, username, userType);
+                });
     }
 
-    /**
-     * Deletes a {@link User} from the {@link com.beanbeanjuice.cafeapi.wrapper.CafeAPI CafeAPI}.
-     * @param username The {@link String username} of the {@link User}.
-     * @return True, if the {@link User} was successfully deleted.
-     */
-    public boolean deleteUser(final String username) {
-        Request request = RequestBuilder.create(RequestRoute.CAFE, RequestType.DELETE)
+    public CompletableFuture<Boolean> deleteUser(final String username) {
+        return RequestBuilder.create(RequestRoute.CAFE, RequestType.DELETE)
                 .setRoute("/user/" + username)
-                .setAuthorization(apiKey).build();
-        return request.getStatusCode() == 200;
+                .setAuthorization(apiKey)
+                .buildAsync()
+                .thenApplyAsync((request) -> request.getStatusCode() == 200);
     }
 
 }
