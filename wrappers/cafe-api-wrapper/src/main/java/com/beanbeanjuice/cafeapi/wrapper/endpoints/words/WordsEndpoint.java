32f18d2e24888f90a6ce4a55c2a740e2d2b5f825
beanbeanjuice <beanbeanjuice@outlook.com>
2024-06-24 06:35:32 -0400

    Added Async to Words Endpoint

--- wrappers/cafe-api-wrapper/src/main/java/com/beanbeanjuice/cafeapi/wrapper/endpoints/words/WordsEndpoint.java
+++ wrappers/cafe-api-wrapper/src/main/java/com/beanbeanjuice/cafeapi/wrapper/endpoints/words/WordsEndpoint.java
@@ -1,78 +1,45 @@
 package com.beanbeanjuice.cafeapi.wrapper.endpoints.words;
 
 import com.beanbeanjuice.cafeapi.wrapper.endpoints.CafeEndpoint;
-import com.beanbeanjuice.cafeapi.wrapper.exception.api.AuthorizationException;
-import com.beanbeanjuice.cafeapi.wrapper.exception.api.NotFoundException;
-import com.beanbeanjuice.cafeapi.wrapper.exception.api.ResponseException;
-import com.beanbeanjuice.cafeapi.wrapper.exception.api.UndefinedVariableException;
-import com.beanbeanjuice.cafeapi.wrapper.requests.Request;
 import com.beanbeanjuice.cafeapi.wrapper.requests.RequestBuilder;
 import com.beanbeanjuice.cafeapi.wrapper.requests.RequestRoute;
 import com.beanbeanjuice.cafeapi.wrapper.requests.RequestType;
 
 import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
 
-/**
- * A class used to retrieve {@link WordsEndpoint} from the database.
- *
- * @author beanbeanjuice
- */
 public class WordsEndpoint extends CafeEndpoint {
 
-    /**
-     * Gets an {@link ArrayList} of {@link Word} from the {@link com.beanbeanjuice.cafeapi.wrapper.CafeAPI CafeAPI}.
-     * @return The {@link ArrayList} of {@link Word} from the {@link com.beanbeanjuice.cafeapi.wrapper.CafeAPI CafeAPI}.
-     * @throws ResponseException Thrown when there is a generic server-side exception.
-     */
-    public ArrayList<Word> getAllWords() throws ResponseException {
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
+    public CompletableFuture<ArrayList<Word>> getAllWords() {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
                 .setRoute("/words")
                 .setAuthorization(apiKey)
-                .build();
+                .buildAsync()
+                .thenApplyAsync((request) -> {
+                    ArrayList<Word> wordList = new ArrayList<>();
+                    request.getData()
+                            .get("words")
+                            .forEach(word -> wordList.add(new Word(word.get("word").asText(), word.get("uses").asInt())));
 
-        ArrayList<Word> wordList = new ArrayList<>();
-        request.getData()
-                .get("words")
-                .forEach(word -> wordList.add(new Word(word.get("word").asText(), word.get("uses").asInt())));
-
-        return wordList;
+                    return wordList;
+                });
     }
 
-    /**
-     * Gets a {@link Word} in the {@link com.beanbeanjuice.cafeapi.wrapper.CafeAPI CafeAPI}.
-     * @param word The {@link String word} to get.
-     * @return The {@link Word} from the {@link com.beanbeanjuice.cafeapi.wrapper.CafeAPI CafeAPI}.
-     * @throws NotFoundException Thrown when the word is not found.
-     * @throws ResponseException Thrown when there is a generic server-side exception.
-     */
-    public Word getWord(final String word) throws NotFoundException, ResponseException {
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
+    public CompletableFuture<Word> getWord(final String word) {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
                 .setRoute("/words/" + word)
                 .setAuthorization(apiKey)
-                .build();
-
-        return new Word(word, request.getData().get("word").get("uses").asInt());
+                .buildAsync()
+                .thenApplyAsync((request) -> new Word(word, request.getData().get("word").get("uses").asInt()));
     }
 
-    /**
-     * Updates a {@link Word} in the {@link com.beanbeanjuice.cafeapi.wrapper.CafeAPI CafeAPI}.
-     * @param word The {@link String word} to be updated.
-     * @param uses The new {@link Integer uses} to be set to.
-     * @return True, if successful.
-     * @throws NotFoundException Thrown when the word was not found.
-     * @throws ResponseException Thrown when there is a generic server-side exception.
-     * @throws AuthorizationException Thrown when the current API key is invalid.
-     * @throws UndefinedVariableException Thrown when a variable is undefined.
-     */
-    public boolean updateWord(final String word, final int uses)
-    throws NotFoundException, ResponseException, AuthorizationException, UndefinedVariableException {
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.PATCH)
+    public CompletableFuture<Boolean> updateWord(final String word, final int uses) {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.PATCH)
                 .setRoute("/words/" + word)
                 .addParameter("uses", String.valueOf(uses))
                 .setAuthorization(apiKey)
-                .build();
-
-        return request.getStatusCode() == 200;
+                .buildAsync()
+                .thenApplyAsync((request) -> request.getStatusCode() == 200);
     }
 
 }
