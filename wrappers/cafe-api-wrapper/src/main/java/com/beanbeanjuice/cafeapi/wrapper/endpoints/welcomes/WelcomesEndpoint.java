822567f37f6e1d42170492f4293365830900595b
beanbeanjuice <beanbeanjuice@outlook.com>
2024-06-24 06:25:12 -0400

    Added Async to Welcomes Endpoint

--- wrappers/cafe-api-wrapper/src/main/java/com/beanbeanjuice/cafeapi/wrapper/endpoints/welcomes/WelcomesEndpoint.java
+++ wrappers/cafe-api-wrapper/src/main/java/com/beanbeanjuice/cafeapi/wrapper/endpoints/welcomes/WelcomesEndpoint.java
@@ -10,120 +10,66 @@ import com.beanbeanjuice.cafeapi.wrapper.exception.api.*;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
 
-/**
- * A class used for the {@link WelcomesEndpoint} API.
- */
 public class WelcomesEndpoint extends CafeEndpoint {
 
-    /**
-     * Retrieves an {@link ArrayList} of {@link GuildWelcome} containing all Guild Welcomes in the {@link CafeAPI CafeAPI}.
-     * @return The {@link ArrayList} of {@link GuildWelcome}.
-     * @throws AuthorizationException Thrown when the api key is unauthorized.
-     * @throws ResponseException Thrown when there is a generic server-side exception.
-     */
-    public ArrayList<GuildWelcome> getAllGuildWelcomes() throws AuthorizationException, ResponseException {
-        ArrayList<GuildWelcome> guildWelcomes = new ArrayList<>();
-
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
+    public CompletableFuture<ArrayList<GuildWelcome>> getAllGuildWelcomes() {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
                 .setRoute("/welcomes")
                 .setAuthorization(apiKey)
-                .build();
+                .buildAsync()
+                .thenApplyAsync((request) -> {
+                    ArrayList<GuildWelcome> guildWelcomes = new ArrayList<>();
 
-        request.getData()
-                .get("welcomes")
-                .forEach((guildWelcome) -> guildWelcomes.add(parseGuildWelcome(guildWelcome)));
+                    request.getData()
+                            .get("welcomes")
+                            .forEach((guildWelcome) -> guildWelcomes.add(parseGuildWelcome(guildWelcome)));
 
-        return guildWelcomes;
+                    return guildWelcomes;
+                });
     }
 
-    /**
-     * Retrieves a {@link GuildWelcome} from the {@link CafeAPI CafeAPI}.
-     * @param guildID The {@link String guildID} to retrieve the {@link GuildWelcome} for.
-     * @return The {@link GuildWelcome} retrieved.
-     * @throws AuthorizationException Thrown when the API key is invalid.
-     * @throws ResponseException Thrown when there is a generic server-side exception.
-     * @throws NotFoundException Thrown when the guild ID is not found.
-     */
-    public GuildWelcome getGuildWelcome(final String guildID)
-    throws AuthorizationException, ResponseException, NotFoundException {
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
+    public CompletableFuture<GuildWelcome> getGuildWelcome(final String guildID) {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.GET)
                 .setRoute("/welcomes/" + guildID)
                 .setAuthorization(apiKey)
-                .build();
-
-        JsonNode guildWelcome = request.getData().get("welcome");
-
-        return parseGuildWelcome(guildWelcome);
+                .buildAsync()
+                .thenApply((request) -> parseGuildWelcome(request.getData().get("welcome")));
     }
 
-    /**
-     * Updates a {@link GuildWelcome} in the {@link CafeAPI CafeAPI}.
-     * @param guildWelcome The new {@link GuildWelcome}.
-     * @return True, if updating the {@link GuildWelcome} was successful.
-     * @throws AuthorizationException Thrown when the API key is invalid.
-     * @throws NotFoundException Thrown when the guild ID is not found.
-     * @throws ResponseException Thrown when there is a generic server-side exception.
-     */
-    public boolean updateGuildWelcome(final GuildWelcome guildWelcome)
-    throws AuthorizationException, NotFoundException, ResponseException {
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.PATCH)
+    public CompletableFuture<Boolean> updateGuildWelcome(final GuildWelcome guildWelcome) {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.PATCH)
                 .setRoute("/welcomes/" + guildWelcome.getGuildID())
                 .addParameter("description", guildWelcome.getDescription().orElse(null))
                 .addParameter("thumbnail_url", guildWelcome.getThumbnailURL().orElse(null))
                 .addParameter("image_url", guildWelcome.getImageURL().orElse(null))
                 .addParameter("message", guildWelcome.getMessage().orElse(null))
                 .setAuthorization(apiKey)
-                .build();
-
-        return request.getStatusCode() == 200;
+                .buildAsync()
+                .thenApplyAsync((request) -> request.getStatusCode() == 200);
     }
 
-    /**
-     * Creates a new {@link GuildWelcome} for the {@link CafeAPI CafeAPI}.
-     * @param guildWelcome The new {@link GuildWelcome} to add.
-     * @return True if the {@link GuildWelcome} was successfully added.
-     * @throws AuthorizationException Thrown when the API key is invalid.
-     * @throws ConflictException Thrown when the provided guild ID already exists.
-     * @throws ResponseException Thrown when there is a generic server-side exception.
-     * @throws UndefinedVariableException Thrown when a variable is undefined.
-     */
-    public boolean createGuildWelcome(final GuildWelcome guildWelcome)
-    throws AuthorizationException, ConflictException, ResponseException, UndefinedVariableException {
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.POST)
+    public CompletableFuture<Boolean> createGuildWelcome(final GuildWelcome guildWelcome) {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.POST)
                 .setRoute("/welcomes/" + guildWelcome.getGuildID())
                 .addParameter("description", guildWelcome.getDescription().orElse(null))
                 .addParameter("thumbnail_url", guildWelcome.getThumbnailURL().orElse(null))
                 .addParameter("image_url", guildWelcome.getImageURL().orElse(null))
                 .addParameter("message", guildWelcome.getMessage().orElse(null))
                 .setAuthorization(apiKey)
-                .build();
-
-        return request.getStatusCode() == 201;
+                .buildAsync()
+                .thenApplyAsync((request) -> request.getStatusCode() == 201);
     }
 
-    /**
-     * Deletes a {@link GuildWelcome} from the {@link CafeAPI CafeAPI}.
-     * @param guildID The {@link String} ID of the {@link GuildWelcome} to delete.
-     * @return True if successfully deleted.
-     * @throws AuthorizationException Thrown when the API key is invalid.
-     * @throws ResponseException Thrown when there is a generic server-side exception.
-     */
-    public boolean deleteGuildWelcome(final String guildID)
-    throws AuthorizationException, ResponseException {
-        Request request = RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.DELETE)
+    public CompletableFuture<Boolean> deleteGuildWelcome(final String guildID) {
+        return RequestBuilder.create(RequestRoute.CAFEBOT, RequestType.DELETE)
                 .setRoute("/welcomes/" + guildID)
                 .setAuthorization(apiKey)
-                .build();
-
-        return request.getStatusCode() == 200;
+                .buildAsync()
+                .thenApplyAsync((request) -> request.getStatusCode() == 200);
     }
 
-    /**
-     * Parses a {@link GuildWelcome} from the {@link JsonNode}.
-     * @param node The {@link JsonNode} to parse.
-     * @return The parsed {@link GuildWelcome}.
-     */
     private GuildWelcome parseGuildWelcome(final JsonNode node) {
         String guildID = node.get("guild_id").asText();
 
--- wrappers/cafe-api-wrapper/src/test/java/com/beanbeanjuice/cafeapi/beta/WelcomesEndpointTests.java
+++ wrappers/cafe-api-wrapper/src/test/java/com/beanbeanjuice/cafeapi/beta/WelcomesEndpointTests.java
@@ -10,20 +10,22 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ExecutionException;
+
 /**
  * A test class used to test all aspects of the {@link WelcomesEndpoint Welcomes} module.
  *
  * @author beanbeanjuice
  */
-public class GuildWelcomeTest {
+public class WelcomesEndpointTests {
 
     @Test
     @DisplayName("Welcomes Endpoint Test")
-    public void testWelcomesEndpoint() {
+    public void testWelcomesEndpoint() throws ExecutionException, InterruptedException {
         CafeAPI cafeAPI = new CafeAPI("beanbeanjuice", System.getenv("API_PASSWORD"), RequestLocation.BETA);
 
         // Makes sure that the amount of guilds in the guild's welcome is more than 0.
-        Assertions.assertTrue(cafeAPI.getWelcomesEndpoint().getAllGuildWelcomes().size() > 0);
+        Assertions.assertFalse(cafeAPI.getWelcomesEndpoint().getAllGuildWelcomes().get().isEmpty());
 
         // Makes sure that the current welcome is set properly.
         Assertions.assertTrue(cafeAPI.getWelcomesEndpoint().updateGuildWelcome(new GuildWelcome(
@@ -32,25 +34,26 @@ public class GuildWelcomeTest {
                 "https://i.pinimg.com/originals/3f/33/75/3f3375eaef9ed7529d0e1bb5b63a814a.gif",
                 "https://i.pinimg.com/originals/3f/33/75/3f3375eaef9ed7529d0e1bb5b63a814a.gif",
                 null
-        )));
+        )).get());
 
         // Makes sure that the first guild is equal to the home guild ID.
-        Assertions.assertEquals("798830792938881024", cafeAPI.getWelcomesEndpoint().getAllGuildWelcomes().get(0).getGuildID());
+        Assertions.assertEquals("798830792938881024", cafeAPI.getWelcomesEndpoint().getAllGuildWelcomes().get().getFirst().getGuildID());
 
         // Makes sure that the description of the retrieved guild is "test description"
         Assertions.assertEquals(
                 "Welcome, {user} to the bot testing server!\\nI hope you enjoy your stay! :heart:\\nMake sure to...\\n Check out #polls,\\nCheck out #logger,\\n and Check out #cafebot-beta-log!",
-                cafeAPI.getWelcomesEndpoint().getGuildWelcome("798830792938881024").getDescription().orElse(null));
+                cafeAPI.getWelcomesEndpoint().getGuildWelcome("798830792938881024").get().getDescription().orElse(null));
 
         // Makes sure that this throws a NotFoundException as the guild "bruhmoment" does not exist.
-        Assertions.assertThrows(NotFoundException.class, () -> {
-            cafeAPI.getWelcomesEndpoint().getGuildWelcome("bruhmoment");
-        });
+        cafeAPI.getWelcomesEndpoint().getGuildWelcome("bruhmoment")
+                .thenApplyAsync((guildWelcome) -> Assertions.fail())
+                .exceptionallyAsync((exception) -> {
+                    Assertions.assertInstanceOf(NotFoundException.class, exception.getCause());
+                    return null;
+                }).join();
 
         // Makes sure that the guild can be updated.
-        Assertions.assertTrue(() -> {
-            GuildWelcome currentGuildWelcome = cafeAPI.getWelcomesEndpoint().getGuildWelcome("798830792938881024");
-
+        cafeAPI.getWelcomesEndpoint().getGuildWelcome("798830792938881024").thenAcceptAsync((currentGuildWelcome) -> {
             GuildWelcome newGuildWelcome = new GuildWelcome(
                     currentGuildWelcome.getGuildID(),
                     currentGuildWelcome.getDescription().orElse(null),
@@ -59,37 +62,40 @@ public class GuildWelcomeTest {
                     "cool message!!!!"
             );
 
-            return cafeAPI.getWelcomesEndpoint().updateGuildWelcome(newGuildWelcome);
+            cafeAPI.getWelcomesEndpoint().updateGuildWelcome(newGuildWelcome)
+                    .thenAcceptAsync(Assertions::assertTrue)
+                    .exceptionallyAsync(Assertions::fail)
+                    .join();
         });
 
         // Throws a conflict exception because the guild welcome cannot be created because it already exists.
-        Assertions.assertThrows(ConflictException.class, () -> {
-            GuildWelcome guildWelcome = new GuildWelcome(
-                    "798830792938881024",
-                    "COOL desc",
-                    "cool url",
-                    "cool image",
-                    null
-            );
-
-            cafeAPI.getWelcomesEndpoint().createGuildWelcome(guildWelcome);
-        });
+        GuildWelcome guildWelcome = new GuildWelcome(
+                "798830792938881024",
+                "COOL desc",
+                "cool url",
+                "cool image",
+                null
+        );
+        cafeAPI.getWelcomesEndpoint().createGuildWelcome(guildWelcome)
+                .thenAcceptAsync((isSuccess) -> Assertions.fail())
+                .exceptionallyAsync((exception) -> {
+                    Assertions.assertInstanceOf(ConflictException.class, exception.getCause());
+                    return null;
+                }).join();
 
         // Makes sure that a guild can be created.
-        Assertions.assertTrue(() -> {
-            GuildWelcome guildWelcome = new GuildWelcome(
-                    "236331890964037632",
-                    "COOL desc",
-                    "cool url",
-                    "cool image",
-                    null
-            );
-
-            return cafeAPI.getWelcomesEndpoint().createGuildWelcome(guildWelcome);
-        });
+        cafeAPI.getWelcomesEndpoint().createGuildWelcome(new GuildWelcome(
+                        "236331890964037632",
+                        "COOL desc",
+                        "cool url",
+                        "cool image",
+                        null
+                )).thenAcceptAsync(Assertions::assertTrue)
+                .exceptionallyAsync(Assertions::fail)
+                .join();
 
         // Is true when a guild welcome is successfully deleted.
-        Assertions.assertTrue(cafeAPI.getWelcomesEndpoint().deleteGuildWelcome("236331890964037632"));
+        Assertions.assertTrue(cafeAPI.getWelcomesEndpoint().deleteGuildWelcome("236331890964037632").get());
     }
 
 }
